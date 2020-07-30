12dacfe2d26ad367b2a5d7b0f458416cb687434b
Khopa <clemguip@gmail.com>
2020-07-30 19:31:38 +0200

    Fixed A-20G payloads.

---
+++ resources/customized_payloads/A-20G.lua
@@ -0,0 +1,72 @@
+local unitPayloads = {
+	["name"] = "A-20G",
+	["payloads"] = {
+		[1] = {
+			["name"] = "ANTISHIP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{4xAN-M64_on_InvCountedAttachmentPoints}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[2] = {
+			["name"] = "STRIKE",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{4xAN-M64_on_InvCountedAttachmentPoints}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[3] = {
+			["name"] = "CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{4xAN-M64_on_InvCountedAttachmentPoints}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[4] = {
+			["name"] = "CAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{4xAN-M64_on_InvCountedAttachmentPoints}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[5] = {
+			["name"] = "SEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{4xAN-M64_on_InvCountedAttachmentPoints}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+	},
+	["tasks"] = {
+		[1] = 32,
+		[2] = 30,
+		[3] = 17,
+	},
+	["unitType"] = "A-20G",
+}
+return unitPayloads
