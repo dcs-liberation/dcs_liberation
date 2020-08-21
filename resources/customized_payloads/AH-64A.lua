cf8060f7ffbc4ff3c8cb0010d222839c1795995d
Khopa <clemguip@gmail.com>
2020-08-21 13:14:03 +0200

    AH-64 better payloads management.

--- resources/customized_payloads/AH-64A.lua
+++ resources/customized_payloads/AH-64A.lua
@@ -10,15 +10,15 @@ local unitPayloads = {
 				},
 				[2] = {
 					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 3,
+					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 2,
+					["CLSID"] = "{FD90A1DC-9147-49FA-BF56-CB83EF0BD32B}",
+					["num"] = 3,
 				},
 				[4] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 1,
+					["CLSID"] = "{FD90A1DC-9147-49FA-BF56-CB83EF0BD32B}",
+					["num"] = 2,
 				},
 			},
 			["tasks"] = {
@@ -36,14 +36,6 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[2] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 3,
-				},
-				[3] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 2,
-				},
-				[4] = {
 					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
 					["num"] = 1,
 				},
@@ -63,14 +55,6 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[2] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 3,
-				},
-				[3] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 2,
-				},
-				[4] = {
 					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
 					["num"] = 1,
 				},
@@ -117,14 +101,6 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[2] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 3,
-				},
-				[3] = {
-					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
-					["num"] = 2,
-				},
-				[4] = {
 					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
 					["num"] = 1,
 				},
