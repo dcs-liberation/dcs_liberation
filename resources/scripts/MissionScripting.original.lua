5f0362be287f71c192a6123f7e784a572afee450
David Pierron <dpierron@gmail.com>
2020-09-23 14:50:59 +0200

    configuration for VEAF

--- resources/scripts/MissionScripting.original.lua
+++ resources/scripts/MissionScripting.original.lua
@@ -2,16 +2,29 @@
 
 dofile('Scripts/ScriptingSystem.lua')
 
+-- Add LuaSocket to the LUAPATH, so that it can be found.
+package.path  = package.path..";.\\LuaSocket\\?.lua;"
+
 --Sanitize Mission Scripting environment
---This makes unavailable some unsecure functions.
+--This makes unavailable some unsecure functions. 
 --Mission downloaded from server to client may contain potentialy harmful lua code that may use these functions.
 --You can remove the code below and make availble these functions at your own risk.
 
 local function sanitizeModule(name)
+	local veafName = "veafSanitized_"..name
+	_G[veafName] = _G[name]
+	package.loaded[veafName] = package.loaded[name]
 	_G[name] = nil
 	package.loaded[name] = nil
 end
 
+do
+	witchcraft = {}
+	witchcraft.host = "localhost"
+	witchcraft.port = 3001
+	dofile(lfs.writedir().."Scripts\\witchcraft.lua")
+end
+	
 do
 	sanitizeModule('os')
 	sanitizeModule('io')
