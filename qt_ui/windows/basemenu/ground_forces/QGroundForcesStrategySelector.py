5adc92c60179eca675f4f75ee0d120b4b1cf5c66
walterroach <37820425+walterroach@users.noreply.github.com>
2020-10-29 23:38:14 -0500

    Properly get enum instead of value on strategy selecter

--- qt_ui/windows/basemenu/ground_forces/QGroundForcesStrategySelector.py
+++ qt_ui/windows/basemenu/ground_forces/QGroundForcesStrategySelector.py
@@ -14,8 +14,8 @@ class QGroundForcesStrategySelector(QComboBox):
             self.cp.stances[enemy_cp.id] = CombatStance.DEFENSIVE
 
         for i, stance in enumerate(CombatStance):
-            self.addItem(stance.name, userData=stance)
-            if self.cp.stances[enemy_cp.id] == stance:
+            self.addItem(stance.name, userData=CombatStance(i))
+            if self.cp.stances[enemy_cp.id] == CombatStance(i):
                 self.setCurrentIndex(i)
 
         self.currentTextChanged.connect(self.on_change)
