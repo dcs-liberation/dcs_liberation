4cff838de0efad2bce6fcb32bfcfded5c752e371
Dan Albert <dan@gingerhq.net>
2020-11-13 18:46:59 -0800

    Develop is now 2.3.

--- game/version.py
+++ game/version.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 #: Current version of Liberation.
-VERSION = "2.2.0-preview"
+VERSION = "2.3.0-preview"
 if Path("buildnumber").exists():
     with open("buildnumber", "r") as file:
         VERSION += f"-{file.readline()}"
