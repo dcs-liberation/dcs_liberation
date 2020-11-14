f8735927bfd5cb4bfa8412802314d996f933b2d1
Khopa <clemguip@gmail.com>
2020-11-14 21:32:27 +0100

    Version string 2.2.0

--- game/version.py
+++ game/version.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 #: Current version of Liberation.
-VERSION = "2.2.0-preview"
+VERSION = "2.2.0"
 if Path("buildnumber").exists():
     with open("buildnumber", "r") as file:
         VERSION += f"-{file.readline()}"
