4b407399182b1889d72a590baafff0d80625348c
Dan Albert <dan@gingerhq.net>
2020-11-14 12:37:22 -0800

    Fix versioning for release builds.
    (cherry picked from commit 9019cbfd2b715c35da10ad7ed79d44ab81e57052)

--- game/version.py
+++ game/version.py
@@ -1,8 +1,17 @@
 from pathlib import Path
 
 
+def _build_version_string() -> str:
+    components = ["2.2.0"]
+    if Path("buildnumber").exists():
+        with open("buildnumber", "r") as file:
+            components.append(file.readline())
+
+    if not Path("final").exists():
+        components.append("preview")
+
+    return "-".join(components)
+
+
 #: Current version of Liberation.
-VERSION = "2.2.0"
-if Path("buildnumber").exists():
-    with open("buildnumber", "r") as file:
-        VERSION += f"-{file.readline()}"
+VERSION = _build_version_string()
