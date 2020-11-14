73a97f9c2ac2ddb51f76b0c1f987399b49a6046f
Khopa <clemguip@gmail.com>
2020-11-14 20:16:13 +0100

    Added High Digit Sams mods pydcs export extensions, maybe for later use.

---
+++ pydcs_extensions/highdigitsams/highdigitsams.py
@@ -0,0 +1,169 @@
+from dcs import unittype
+
+
+class SAM_SA_20_S_300PMU1_TR_30N6E(unittype.VehicleType):
+    id = "S-300PMU1 40B6M tr"
+    name = "SAM SA-20 S-300PMU1 TR 30N6E"
+    detection_range = 160000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_20_S_300PMU1_TR_30N6E_truck(unittype.VehicleType):
+    id = "S-300PMU1 30N6E tr"
+    name = "SAM SA-20 S-300PMU1 TR 30N6E(truck)"
+    detection_range = 160000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_20_S_300PMU1_SR_5N66E(unittype.VehicleType):
+    id = "S-300PMU1 40B6MD sr"
+    name = "SAM SA-20 S-300PMU1 SR 5N66E"
+    detection_range = 120000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_20_S_300PMU1_SR_64N6E(unittype.VehicleType):
+    id = "S-300PMU1 64N6E sr"
+    name = "SAM SA-20 S-300PMU1 SR 64N6E"
+    detection_range = 300000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_23_S_300VM_9S15M2_SR(unittype.VehicleType):
+    id = "S-300VM 9S15M2 sr"
+    name = "SAM SA-23 S-300VM 9S15M2 SR"
+    detection_range = 320000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_23_S_300VM_9S19M2_SR(unittype.VehicleType):
+    id = "S-300VM 9S19M2 sr"
+    name = "SAM SA-23 S-300VM 9S19M2 SR"
+    detection_range = 310000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_23_S_300VM_9S32ME_TR(unittype.VehicleType):
+    id = "S-300VM 9S32ME tr"
+    name = "SAM SA-23 S-300VM 9S32ME TR"
+    detection_range = 230000
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_20_S_300PMU1_LN_5P85CE(unittype.VehicleType):
+    id = "S-300PMU1 5P85CE ln"
+    name = "SAM SA-20 S-300PMU1 LN 5P85CE"
+    detection_range = 0
+    threat_range = 150000
+    air_weapon_dist = 150000
+
+
+class SAM_SA_20_S_300PMU1_LN_5P85DE(unittype.VehicleType):
+    id = "S-300PMU1 5P85DE ln"
+    name = "SAM SA-20 S-300PMU1 LN 5P85DE"
+    detection_range = 0
+    threat_range = 150000
+    air_weapon_dist = 150000
+
+
+class SAM_SA_10__5V55RUD__S_300PS_LN_5P85CE(unittype.VehicleType):
+    id = "S-300PS 5P85CE ln"
+    name = "SAM SA-10 (5V55RUD) S-300PS LN 5P85CE"
+    detection_range = 0
+    threat_range = 90000
+    air_weapon_dist = 90000
+
+
+class SAM_SA_10__5V55RUD__S_300PS_LN_5P85DE(unittype.VehicleType):
+    id = "S-300PS 5P85DE ln"
+    name = "SAM SA-10 (5V55RUD) S-300PS LN 5P85DE"
+    detection_range = 0
+    threat_range = 90000
+    air_weapon_dist = 90000
+
+
+class SAM_SA_23_S_300VM_9A83ME_LN(unittype.VehicleType):
+    id = "S-300VM 9A83ME ln"
+    name = "SAM SA-23 S-300VM 9A83ME LN"
+    detection_range = 0
+    threat_range = 90000
+    air_weapon_dist = 90000
+
+
+class SAM_SA_23_S_300VM_9A82ME_LN(unittype.VehicleType):
+    id = "S-300VM 9A82ME ln"
+    name = "SAM SA-23 S-300VM 9A82ME LN"
+    detection_range = 0
+    threat_range = 200000
+    air_weapon_dist = 200000
+
+
+class SAM_SA_17_Buk_M1_2_LN_9A310M1_2(unittype.VehicleType):
+    id = "SA-17 Buk M1-2 LN 9A310M1-2"
+    name = "SAM SA-17 Buk M1-2 LN 9A310M1-2"
+    detection_range = 120000
+    threat_range = 50000
+    air_weapon_dist = 50000
+
+
+class SAM_SA_2__V759__LN_SM_90(unittype.VehicleType):
+    id = "S_75M_Volhov_V759"
+    name = "SAM SA-2 (V759) LN SM-90"
+    detection_range = 0
+    threat_range = 50000
+    air_weapon_dist = 50000
+
+
+class SAM_HQ_2_LN_SM_90(unittype.VehicleType):
+    id = "HQ_2_Guideline_LN"
+    name = "SAM HQ-2 LN SM-90"
+    detection_range = 0
+    threat_range = 50000
+    air_weapon_dist = 50000
+
+
+class SAM_SA_3__V_601P__LN_5P73(unittype.VehicleType):
+    id = "5p73 V-601P ln"
+    name = "SAM SA-3 (V-601P) LN 5P73"
+    detection_range = 0
+    threat_range = 18000
+    air_weapon_dist = 18000
+
+
+class SAM_SA_20_S_300PMU1_CP_54K6(unittype.VehicleType):
+    id = "S-300PMU1 54K6 cp"
+    name = "SAM SA-20 S-300PMU1 CP 54K6"
+    detection_range = 0
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_23_S_300VM_9S457ME_CP(unittype.VehicleType):
+    id = "S-300VM 9S457ME cp"
+    name = "SAM SA-23 S-300VM 9S457ME CP"
+    detection_range = 0
+    threat_range = 0
+    air_weapon_dist = 0
+
+
+class SAM_SA_24_Igla_S_manpad(unittype.VehicleType):
+    id = "SA-24 Igla-S manpad"
+    name = "SAM SA-24 Igla-S manpad"
+    detection_range = 5000
+    threat_range = 6000
+    air_weapon_dist = 6000
+
+
+class SAM_SA_14_Strela_3_manpad(unittype.VehicleType):
+    id = "SA-14 Strela-3 manpad"
+    name = "SAM SA-14 Strela-3 manpad"
+    detection_range = 5000
+    threat_range = 4500
+    air_weapon_dist = 4500
