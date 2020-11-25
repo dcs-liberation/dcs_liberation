fc3eb9215f417bd619bd0104de5a3df1f20aec52
Ignacio Muñoz Fernandez <nmunozfernandez@gmail.com>
2020-11-25 11:50:12 +0100

    wip: fixed qt cosmetic issue, added forecast text generation, initial for weather window

--- qt_ui/widgets/QConditionsWidget.py
+++ qt_ui/widgets/QConditionsWidget.py
@@ -1,9 +1,16 @@
 import datetime
 
-from PySide2.QtWidgets import QLabel, QHBoxLayout, QGroupBox, QVBoxLayout, QFrame, QSizePolicy, QStyle, QPushButton
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QLabel, QHBoxLayout, QGroupBox, QVBoxLayout, QFrame, QSizePolicy, QStyle, QPushButton, QGridLayout
+from PySide2.QtGui import QFont
 
-from game.weather import Conditions, TimeOfDay
+from game.weather import Conditions, TimeOfDay, Weather
+from dcs.weather import Weather as PydcsWeather
+
+from qt_ui.windows.weather.QWeatherInfoWindow import QWeatherInfoWindow
 import qt_ui.uiconstants as CONST
+
+
 
 class QTimeTurnWidget(QGroupBox):
     """
@@ -54,10 +61,14 @@ class QWeatherWidget(QGroupBox):
     """
     UI Component to display current weather forecast
     """
+    turn = None
+    conditions = None
 
     def __init__(self):
         super(QWeatherWidget, self).__init__("")
         self.setProperty('style', 'QWeatherWidget')
+        self.conditions = None
+        
 
         self.icons = {
             TimeOfDay.Dawn: CONST.ICONS["Dawn"],
@@ -68,14 +79,22 @@ class QWeatherWidget(QGroupBox):
 
         self.layout = QHBoxLayout()
         self.setLayout(self.layout)
-        
 
         self.weather_icon = QLabel()
         self.weather_icon.setPixmap(self.icons[TimeOfDay.Dawn])
         self.layout.addWidget(self.weather_icon)
 
-        self.forecast = QLabel('')
-        self.layout.addWidget(self.forecast)
+        self.textLayout = QVBoxLayout()
+        self.layout.addLayout(self.textLayout)
+
+        self.forecastClouds = QLabel('')
+        self.textLayout.addWidget(self.forecastClouds)
+
+        self.forecastRain = QLabel('')
+        self.textLayout.addWidget(self.forecastRain)
+
+        self.forecastFog = QLabel('')
+        self.textLayout.addWidget(self.forecastFog)
 
         self.details = QPushButton("Weather")
         self.details.setProperty("style", "btn-primary")
@@ -84,14 +103,21 @@ class QWeatherWidget(QGroupBox):
         self.layout.addWidget(self.details)
 
     def openDetailWindow(self):
-        pass
+        self.subwindow = QWeatherInfoWindow(self.turn, self.conditions)
+        self.subwindow.show()
 
-    def setCurrentConditions(self, conditions: Conditions) -> None:
+    def setCurrentTurn(self, turn: int, conditions: Conditions) -> None:
         """Sets the turn information display.
 
         :arg conditions Current time and weather conditions.
         """
+        self.turn
         self.conditions = conditions
+
+        if conditions and turn > 0:
+            self.details.setDisabled(True)
+        else:
+            self.details.setDisabled(False)
 
         self.updateIcon()
         self.updateText()
@@ -110,31 +136,60 @@ class QWeatherWidget(QGroupBox):
         """
         Updates the Forecast Text based on turn conditions
         """
+        cloudDensity = self.conditions.weather.clouds.density
+        precipitation = self.conditions.weather.clouds.precipitation
 
-        pass
+        if cloudDensity <= 0:
+            self.forecastClouds.setText('Sunny')
+        
+        if cloudDensity > 0 and cloudDensity < 3:
+            self.forecastClouds.setText('Partly Cloudy')
+
+        if cloudDensity > 3 and cloudDensity < 6:
+            self.forecastClouds.setText('Mostly Cloudy')
+
+        if cloudDensity > 6:
+            self.forecastClouds.setText('Totally Cloudy')
+        
+
+        if precipitation == PydcsWeather.Preceptions.Rain:
+            self.forecastRain.setText('Rain')
+        elif precipitation == PydcsWeather.Preceptions.Thunderstorm:
+            self.forecastRain.setText('Thunderstorm')
+        else:
+            self.forecastRain.setText('No Rain')
+
+        if not self.conditions.weather.fog:
+            self.textLayout.removeWidget(self.forecastFog)
+
 
     def updateDetailsBtn(self):        
         if not self.conditions:
             self.details.setEnable(False)
         else:
             self.details.setEnable(True)
-        
+
 
 class QConditionsWidget(QFrame):
     def __init__(self):
         super(QConditionsWidget, self).__init__()
-        self.layout = QHBoxLayout()        
-        self.layout.setContentsMargins(0, 0, 0, 0)
-        self.setLayout(self.layout)
         self.setProperty('style', 'QConditionsWidget')
 
+        self.layout = QGridLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setHorizontalSpacing(0)
+        self.layout.setVerticalSpacing(0)
+        self.setLayout(self.layout)
+
         self.time_turn_widget = QTimeTurnWidget()
-        self.layout.addWidget(self.time_turn_widget)
+        self.time_turn_widget.setStyleSheet('QGroupBox { margin-right: 0px; border-right: 0px; }')
+        self.layout.addWidget(self.time_turn_widget, 0, 0)
 
         self.weather_widget = QWeatherWidget()
-        self.layout.addWidget(self.weather_widget)
+        self.weather_widget.setStyleSheet('QGroupBox { margin-top: 5px; margin-left: 0px; }')
+        self.layout.addWidget(self.weather_widget, 0, 1)
 
     def setCurrentTurn(self, turn: int, conditions: Conditions) -> None:
         self.time_turn_widget.setCurrentTurn(turn, conditions)
-        self.weather_widget.setCurrentConditions(conditions)
+        self.weather_widget.setCurrentTurn(turn, conditions)
 
