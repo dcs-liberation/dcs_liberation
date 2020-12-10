8a01209ded308af429e825e48b23e3c1f9138505
Dan Albert <dan@gingerhq.net>
2020-12-09 20:35:30 -0800

    Add data for lat/lon conversions.

---
+++ game/theater/syria.py
@@ -0,0 +1,8 @@
+from game.theater.projections import TransverseMercator
+
+PARAMETERS = TransverseMercator(
+    central_meridian=39,
+    false_easting=282801.00000003993,
+    false_northing=-3879865.9999999935,
+    scale_factor=0.9996,
+)
