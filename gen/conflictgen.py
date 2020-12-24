3fd2e8d807ce3936accb312d08f587b9db58e28d
walterroach <37820425+walterroach@users.noreply.github.com>
2020-12-24 12:40:37 -0600

    Change frontline vector for old behavior

--- gen/conflictgen.py
+++ gen/conflictgen.py
@@ -93,11 +93,13 @@ class Conflict:
         line = LineString([p0, p1])
 
         intersection = line.intersection(
-            theater.landmap.inclusion_zones.boundary)
+            theater.landmap.exclusion_zones.boundary)
         if intersection.is_empty:
+            intersection = line.intersection(theater.landmap.sea_zones.boundary)
             # Max extent does not intersect with the boundary of the inclusion
             # zone, so the full front line is usable. This does assume that the
             # front line was centered on a valid location.
+        if intersection.is_empty:
             return extended
 
         # Otherwise extend the front line only up to the intersection.
