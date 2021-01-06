f2e35c185b10f0ad865ce749fa01973c472350b0
Simon Clark <simonc6r@gmail.com>
2021-01-06 12:47:15 +0000

    Displays the default loadout in the payload UI.
    The default loadout is now displayed whenever the custom loadout checkbox is unchecked.
    
    The custom loadout is reset when the custom loadout is checked, to force the user to be prescriptive about the loadout.
    
    Contributes-to: Khopa/dcs_liberation#725

--- qt_ui/windows/mission/flight/payload/QLoadoutEditor.py
+++ qt_ui/windows/mission/flight/payload/QLoadoutEditor.py
@@ -39,5 +39,14 @@ class QLoadoutEditor(QGroupBox):
 
     def on_toggle(self):
         self.flight.use_custom_loadout = self.isChecked()
-
-
+        if not self.isChecked():
+            for i in self.findChildren(QPylonEditor):
+                for j in enumerate(Pylon.iter_pylons(self.flight.unit_type)):
+                    # Only change the text in the pylon selector, not any actual data.
+                    i.default_loadout(j[0])
+        else:
+            for i in self.findChildren(QPylonEditor):
+                # Clear the loadout so that the user has select it themself.
+                # If we don't do this, the user may end up leaving an entry as the default,
+                # which is just text, not actual data, meaning they'd have an empty pylon.
+                i.clear_loadout()
