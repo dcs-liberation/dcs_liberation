3e1547e0da30e79d44906814c5d67ef21c58c17f
Khopa <clemguip@gmail.com>
2021-01-27 22:29:43 +0100

    Harrier default CAS loadout has APKWS

--- resources/customized_payloads/AV8BNA.lua
+++ resources/customized_payloads/AV8BNA.lua
@@ -2,86 +2,6 @@ local unitPayloads = {
 	["name"] = "AV8BNA",
 	["payloads"] = {
 		[1] = {
-			["name"] = "CAP",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 8,
-				},
-				[2] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 1,
-				},
-				[3] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
-					["num"] = 2,
-				},
-				[4] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
-					["num"] = 7,
-				},
-				[5] = {
-					["CLSID"] = "{GAU_12_Equalizer}",
-					["num"] = 4,
-				},
-			},
-			["tasks"] = {
-				[1] = 31,
-			},
-		},
-		[2] = {
-			["name"] = "CAS",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 8,
-				},
-				[2] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 1,
-				},
-				[3] = {
-					["CLSID"] = "LAU_117_AGM_65G",
-					["num"] = 2,
-				},
-				[4] = {
-					["CLSID"] = "LAU_117_AGM_65G",
-					["num"] = 7,
-				},
-				[5] = {
-					["CLSID"] = "{GAU_12_Equalizer}",
-					["num"] = 4,
-				},
-				[6] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 5,
-				},
-			},
-			["tasks"] = {
-				[1] = 31,
-			},
-		},
-		[3] = {
-			["name"] = "STRIKE",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{BRU-42_2*GBU-38_LEFT}",
-					["num"] = 2,
-				},
-				[2] = {
-					["CLSID"] = "{BRU-42_2*GBU-38_RIGHT}",
-					["num"] = 7,
-				},
-				[3] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 5,
-				},
-			},
-			["tasks"] = {
-				[1] = 31,
-			},
-		},
-		[4] = {
 			["name"] = "ANTISHIP",
 			["pylons"] = {
 				[1] = {
@@ -93,51 +13,31 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "LAU_117_AGM_65G",
+					["CLSID"] = "LAU_117_AGM_65F",
 					["num"] = 2,
 				},
 				[4] = {
-					["CLSID"] = "LAU_117_AGM_65G",
+					["CLSID"] = "LAU_117_AGM_65F",
 					["num"] = 7,
 				},
 				[5] = {
 					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
 					["num"] = 5,
 				},
-			},
-			["tasks"] = {
-				[1] = 31,
-			},
-		},
-		[5] = {
-			["name"] = "INTERCEPT",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 8,
+				[6] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 6,
 				},
-				[2] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 1,
-				},
-				[3] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
-					["num"] = 2,
-				},
-				[4] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
-					["num"] = 7,
-				},
-				[5] = {
-					["CLSID"] = "{GAU_12_Equalizer}",
-					["num"] = 4,
+				[7] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 3,
 				},
 			},
 			["tasks"] = {
 				[1] = 31,
 			},
 		},
-		[6] = {
+		[2] = {
 			["name"] = "SEAD",
 			["pylons"] = {
 				[1] = {
@@ -167,6 +67,122 @@ local unitPayloads = {
 				[7] = {
 					["CLSID"] = "LAU_117_AGM_65G",
 					["num"] = 3,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[3] = {
+			["name"] = "CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{GAU_12_Equalizer}",
+					["num"] = 4,
+				},
+				[6] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
+				},
+				[7] = {
+					["CLSID"] = "{LAU-131 - 7 AGR-20 M282}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{LAU-131 - 7 AGR-20 M282}",
+					["num"] = 3,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[4] = {
+			["name"] = "STRIKE",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{BRU-42_2*GBU-38_LEFT}",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "{BRU-42_2*GBU-38_RIGHT}",
+					["num"] = 7,
+				},
+				[3] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[5] = {
+			["name"] = "CAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{GAU_12_Equalizer}",
+					["num"] = 4,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[6] = {
+			["name"] = "INTERCEPT",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{GAU_12_Equalizer}",
+					["num"] = 4,
 				},
 			},
 			["tasks"] = {
