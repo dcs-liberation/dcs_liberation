3d0b47a1816ecf83b32900fb556e8ba443fca9c7
Dan Albert <dan@gingerhq.net>
2021-01-30 18:24:25 -0800

    Fix exception in building debrief.
    There are always a bunch of integer dead ground units. Not sure what
    they are.

--- game/debriefing.py
+++ game/debriefing.py
@@ -199,7 +199,9 @@ class Debriefing:
             building = self.unit_map.building_or_fortification(unit_name)
             # Try appending object to the name, because we do this for building statics.
             if building is None:
-                building = self.unit_map.building_or_fortification(unit_name + " object")
+                building = self.unit_map.building_or_fortification(
+                    f"{unit_name} object"
+                )
             if building is not None:
                 if building.ground_object.control_point.captured:
                     losses.player_buildings.append(building)
