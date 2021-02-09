56012733106732ec7cb08ab85852908ea5f7c718
Brandon Danyluk <brandondanyluk366@gmail.com>
2021-02-08 23:22:38 -0700

    change casing

--- game/data/weapons.py
+++ game/data/weapons.py
@@ -403,7 +403,7 @@ _WEAPON_FALLBACKS = [
     (Weapons.CBU_105, Weapons.CBU_97),
     
     (Weapons.LAU_131_pod___7_x_2_75__Hydra___Laser_Guided_Rkts_M151___HE_APKWS, Weapons.LAU_131___7_2_75__rockets_M151__HE_),
-    (Weapons.LAU_131_pod___7_x_2_75__Hydra___Laser_Guided_Rkts_M282___MPP_APKWS, WEapons.LAU_131___7_2_75__rockets_M151__HE_),
+    (Weapons.LAU_131_pod___7_x_2_75__Hydra___Laser_Guided_Rkts_M282___MPP_APKWS, Weapons.LAU_131___7_2_75__rockets_M151__HE_),
     (Weapons._3_x_LAU_131_pods___21_x_2_75__Hydra___Laser_Guided_M151___HE_APKWS, Weapons.LAU_68_3___7_2_75__rockets_M151__HE_),
     (Weapons._3_x_LAU_131_pods___21_x_2_75__Hydra___Laser_Guided_M282___MPP_APKWS, Weapons.LAU_68_3___7_2_75__rockets_M151__HE_),
     
