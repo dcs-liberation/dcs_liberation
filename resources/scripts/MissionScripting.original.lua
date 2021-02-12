0ae6575087bed637b71f3c4eaef6be9934263b4c
Dan Albert <dan@gingerhq.net>
2021-02-12 13:43:30 -0800

    Transfer pending purchases forward along capture.
    Fixes https://github.com/Khopa/dcs_liberation/issues/828.
    
    (cherry picked from commit d8c94f5ecee6b136706d40f00c13f752dccac6e1)

--- resources/scripts/MissionScripting.original.lua
+++ resources/scripts/MissionScripting.original.lua
@@ -3,7 +3,7 @@
 dofile('Scripts/ScriptingSystem.lua')
 
 --Sanitize Mission Scripting environment
---This makes unavailable some unsecure functions. 
+--This makes unavailable some unsecure functions.
 --Mission downloaded from server to client may contain potentialy harmful lua code that may use these functions.
 --You can remove the code below and make availble these functions at your own risk.
 
