49b341e41b27b223745b6939b11c9d87ebb9dff7
Dan Albert <dan@gingerhq.net>
2021-02-21 13:23:55 -0800

    Black.

--- gen/kneeboard.py
+++ gen/kneeboard.py
@@ -249,7 +249,7 @@ class BriefingPage(KneeboardPage):
     def write(self, path: Path) -> None:
         writer = KneeboardPageWriter()
         if self.flight.custom_name is not None:
-            custom_name_title = " (\"{}\")".format(self.flight.custom_name)
+            custom_name_title = ' ("{}")'.format(self.flight.custom_name)
         else:
             custom_name_title = ""
         writer.title(f"{self.flight.callsign} Mission Info{custom_name_title}")
