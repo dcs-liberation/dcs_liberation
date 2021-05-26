8d8163ea45083e969efbb595f6acf760c9b5c92e
Brock Greman <bgreman@gmail.com>
2021-05-26 09:34:32 -0400

    Commenting injector method

--- pydcs_extensions/weapon_injector.py
+++ pydcs_extensions/weapon_injector.py
@@ -4,6 +4,11 @@ from dcs.weapons_data import Weapons, weapon_ids
 
 
 def inject_weapons(weapon_class: Any) -> None:
+    """
+    Inject custom weapons from mods into pydcs weapons databases via introspection
+    :param weapon_class: The custom custom weapons class containing dictionaries with weapon info
+    :return: None
+    """
     for key, value in weapon_class.__dict__.items():
         if key.startswith("__"):
             continue
