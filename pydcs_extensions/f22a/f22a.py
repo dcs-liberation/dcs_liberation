c163e2c981035ec9cd22c5a54ea4f1d9a3ffbb8d
bgreman <47828384+bgreman@users.noreply.github.com>
2021-05-26 15:49:49 -0400

    Inject mod plane weapons into pydcs.
    Adds a simple injector that iterates over attrs of an input class and
    injects things that look like custom weapons into pydcs's weapons
    classes.
    
    Also updated all current mod aircraft configs to perform the injection.

--- pydcs_extensions/f22a/f22a.py
+++ pydcs_extensions/f22a/f22a.py
@@ -2,16 +2,17 @@ from enum import Enum
 
 from dcs import task
 from dcs.planes import PlaneType
-from dcs.weapons_data import Weapons, weapon_ids
+from dcs.weapons_data import Weapons
+
+from pydcs_extensions.weapon_injector import inject_weapons
 
 
 class F22AWeapons:
     AIM_9XX = {"clsid": "{AIM-9XX}", "name": "AIM-9XX", "weight": 85}
     AIM_120D = {"clsid": "{AIM-120D}", "name": "AIM-120D", "weight": 152}
-    Weapons.AIM_9XX = AIM_9XX
-    Weapons.AIM_120D = AIM_120D
-    weapon_ids["{AIM-9XX}"] = AIM_9XX
-    weapon_ids["{AIM-120D}"] = AIM_120D
+
+
+inject_weapons(F22AWeapons)
 
 
 class F_22A(PlaneType):
