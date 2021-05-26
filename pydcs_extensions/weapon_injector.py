2a484eddedeb9be07f7ffc577a4089c2b26eea02
Brock Greman <bgreman@gmail.com>
2021-05-26 09:36:34 -0400

    Fixing comment

--- pydcs_extensions/weapon_injector.py
+++ pydcs_extensions/weapon_injector.py
@@ -6,7 +6,7 @@ from dcs.weapons_data import Weapons, weapon_ids
 def inject_weapons(weapon_class: Any) -> None:
     """
     Inject custom weapons from mods into pydcs weapons databases via introspection
-    :param weapon_class: The custom custom weapons class containing dictionaries with weapon info
+    :param weapon_class: The custom weapons class containing dictionaries with weapon info
     :return: None
     """
     for key, value in weapon_class.__dict__.items():
