794de0fcbbd8b3b67b384e65d60ab9f0c9f25837
Florian <floheid@7fun.de>
2021-06-10 00:05:43 +0200

    added missing units to price table
    (cherry picked from commit 366190ee99fb6c25e45643862e61a6c6bf1f604d)

--- game/db.py
+++ game/db.py
@@ -574,6 +574,12 @@ PRICES = {
     Armor.Car_Daimler_Armored: 8,
     Armor.LT_Mk_VII_Tetrarch: 8,
     Unarmed.Tractor_M4_Hi_Speed: 2,
+    Unarmed.Carrier_Sd_Kfz_7_Tractor: 1,
+    Unarmed.LUV_Kettenrad: 1,
+    Unarmed.LUV_Kubelwagen_82: 1,
+    Unarmed.Truck_Opel_Blitz: 1,
+    Unarmed.Truck_Bedford: 1,
+    Unarmed.Truck_GMC_Jimmy_6x6_Truck: 1,
     # ship
     CV_1143_5_Admiral_Kuznetsov: 100,
     CVN_74_John_C__Stennis: 100,
