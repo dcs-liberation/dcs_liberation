b0297a337f38a1df7f9746c3fe4a837158714591
Florian <floheid@7fun.de>
2021-06-21 23:02:47 +0200

    adding shorad to armorgroups > 4 vehicles

--- gen/defenses/armor_group_generator.py
+++ gen/defenses/armor_group_generator.py
@@ -28,12 +28,15 @@ def generate_armor_group(faction: str, game, ground_object):
     ]
     if len(possible_unit) > 0:
         unit_type = random.choice(possible_unit)
-        return generate_armor_group_of_type(game, ground_object, unit_type)
+        return generate_armor_group_of_type(game, ground_object, unit_type, faction)
     return None
 
 
 def generate_armor_group_of_type(
-    game: Game, ground_object: VehicleGroupGroundObject, unit_type: GroundUnitType
+    game: Game,
+    ground_object: VehicleGroupGroundObject,
+    unit_type: GroundUnitType,
+    faction: str,
 ) -> VehicleGroup:
     """
     This generate a group of ground units of given type
@@ -41,7 +44,29 @@ def generate_armor_group_of_type(
     """
     generator = ArmoredGroupGenerator(game, ground_object, unit_type)
     generator.generate()
-    return generator.get_generated_group()
+
+    vehicle_group = generator.get_generated_group()
+
+    if len(vehicle_group.units) >= 4:
+        shorads = [
+            u
+            for u in db.FACTIONS[faction].frontline_units
+            if u.unit_class == GroundUnitClass.Shorads
+        ]
+        if len(shorads) > 0:
+            shorad = random.choice(shorads)
+            spacing = (
+                vehicle_group.units[0].position.x - vehicle_group.units[1].position.x
+            )
+            generator.add_unit(
+                shorad.dcs_unit_type,
+                "group-shorad",
+                vehicle_group.units[0].position.x - spacing,
+                vehicle_group.units[0].position.y - spacing,
+                vehicle_group.units[0].heading,
+            )
+
+    return vehicle_group
 
 
 def generate_armor_group_of_type_and_size(
