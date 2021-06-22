c227923bdf4a842e0591695fe8be96a27689fad2
Chris Seagraves <47610393+nosv1@users.noreply.github.com>
2021-06-22 16:19:53 -0500

    Fix bug with file name in title with invalid save games.
    (cherry picked from commit 814519248c4a46315bd7162f1165a893dc5ade6c)

--- qt_ui/windows/QLiberationWindow.py
+++ qt_ui/windows/QLiberationWindow.py
@@ -71,7 +71,7 @@ class QLiberationWindow(QMainWindow):
                     logging.info("Loading last saved game : " + str(last_save_file))
                     game = persistency.load_game(last_save_file)
                     self.onGameGenerated(game)
-                    self.updateWindowTitle(last_save_file)
+                    self.updateWindowTitle(last_save_file if game else None)
                 except:
                     logging.info("Error loading latest save game")
             else:
