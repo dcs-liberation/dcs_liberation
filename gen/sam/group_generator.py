e78deaf2faf1d26c0a35e2fec3c61d8d12554d7b
Brock Greman <bgreman@gmail.com>
2021-07-09 12:38:16 -0400

    Fixing broken group generation.

--- gen/sam/group_generator.py
+++ gen/sam/group_generator.py
@@ -77,7 +77,7 @@ class VehicleGroupGenerator(
         super().__init__(
             game,
             ground_object,
-            unitgroup.VehicleGroup(self.game.next_group_id(), self.go.group_name),
+            unitgroup.VehicleGroup(game.next_group_id(), ground_object.group_name),
         )
         wp = self.vg.add_waypoint(self.position, PointAction.OffRoad, 0)
         wp.ETA_locked = True
