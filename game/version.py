2295f4edfe634ffdbc63ca0383910b79bc185a6b
Dan Albert <dan@gingerhq.net>
2021-07-21 17:10:29 -0700

    Bump campaign version to 8.0 for latest DCS.
    Building IDs changed again. Ack the change in my two campaigns which
    don't use these target types.
    
    (cherry picked from commit edbd3de4a4f8d62adeb98880f97e012d2bab75a8)

--- game/version.py
+++ game/version.py
@@ -106,4 +106,8 @@ VERSION = _build_version_string()
 #:
 #: Version 7.1
 #: * Support for Mariana Islands terrain
-CAMPAIGN_FORMAT_VERSION = (7, 1)
+#:
+#: Version 8.0
+#: * DCS 2.7.4.9632 changed scenery target IDs. Any mission using map buildings as
+#:   strike targets must check and potentially recreate all those objectives.
+CAMPAIGN_FORMAT_VERSION = (8, 0)
