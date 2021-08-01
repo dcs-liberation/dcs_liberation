3dc71a558dea33ce39e402d380abeb82b4abc19d
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2021-08-01 20:33:21 +0200

    Estimate preferred patrol altitude based on max speed
    (cherry picked from commit a3e3e9046f4c629114e8f46e5ef6b6718c6852c2)

--- gen/flights/flightplan.py
+++ gen/flights/flightplan.py
@@ -1168,11 +1168,12 @@ class FlightPlanBuilder:
             raise InvalidObjectiveLocation(flight.flight_type, location)
 
         start_pos, end_pos = self.racetrack_for_objective(location, barcap=True)
-        patrol_alt = meters(
-            random.randint(
-                int(self.doctrine.min_patrol_altitude.meters),
-                int(self.doctrine.max_patrol_altitude.meters),
-            )
+
+        preferred_alt = flight.unit_type.preferred_patrol_altitude
+        randomized_alt = preferred_alt + feet(random.randint(-2, 1) * 1000)
+        patrol_alt = max(
+            self.doctrine.min_patrol_altitude,
+            min(self.doctrine.max_patrol_altitude, randomized_alt),
         )
 
         builder = WaypointBuilder(flight, self.game, self.is_player)
@@ -1406,11 +1407,11 @@ class FlightPlanBuilder:
         """
         location = self.package.target
 
-        patrol_alt = meters(
-            random.randint(
-                int(self.doctrine.min_patrol_altitude.meters),
-                int(self.doctrine.max_patrol_altitude.meters),
-            )
+        preferred_alt = flight.unit_type.preferred_patrol_altitude
+        randomized_alt = preferred_alt + feet(random.randint(-2, 1) * 1000)
+        patrol_alt = max(
+            self.doctrine.min_patrol_altitude,
+            min(self.doctrine.max_patrol_altitude, randomized_alt),
         )
 
         # Create points
