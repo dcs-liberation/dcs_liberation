8404a576b553ed93f468ba1a168b3d81913227a2
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2021-08-09 00:06:31 +0200

    Don't always use 2-ship barcap, since it becomes predictable

--- game/commander/tasks/primitive/barcap.py
+++ game/commander/tasks/primitive/barcap.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+import random
 
 from game.commander.tasks.packageplanningtask import PackagePlanningTask
 from game.commander.theaterstate import TheaterState
@@ -21,7 +22,15 @@ class PlanBarcap(PackagePlanningTask[ControlPoint]):
         state.barcaps_needed[self.target] -= 1
 
     def propose_flights(self) -> None:
-        self.propose_flight(FlightType.BARCAP, 2)
+        chances = {
+            2: 75,
+            3: 5,
+            4: 20,
+        }
+        num_aircraft = random.choices(
+            list(chances.keys()), weights=list(chances.values())
+        )[0]
+        self.propose_flight(FlightType.BARCAP, num_aircraft)
 
     @property
     def purchase_multiplier(self) -> int:
