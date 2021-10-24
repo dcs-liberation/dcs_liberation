dc913f25429ce3ef00b6d5a00506f312c79e8c77
Ghosti <ghosti@smytky.org>
2021-10-24 03:08:10 +0300

    Revert "Moved default_start_type_client from packagebuilder.py to flight.py as suggested."
    This reverts commit 86b4994d714dd5caa92806cdc14cd2bd8a2a70e9, reversing
    changes made to 3f957d4d1b8af9526582ccd9825a1ed171820b35.

--- game/commander/packagebuilder.py
+++ game/commander/packagebuilder.py
@@ -56,7 +56,6 @@ class PackageBuilder:
             start_type = self.start_type
 
         flight = Flight(
-            self.settings,
             self.package,
             self.package_country,
             squadron,
@@ -65,6 +64,11 @@ class PackageBuilder:
             start_type,
             divert=self.find_divert_field(squadron.aircraft, squadron.location),
         )
+
+        # If this is a client flight, set the start_type again to match the configured default
+        # https://github.com/dcs-liberation/dcs_liberation/issues/1567
+        if flight.roster is not None and flight.roster.player_count > 0:
+            flight.start_type = self.settings.default_start_type_client
 
         self.package.add_flight(flight)
         return True
