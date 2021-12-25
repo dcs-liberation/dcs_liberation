568457088012f382ea8bc2e233ab84c9ed1f2f93
MetalStormGhost <89945461+MetalStormGhost@users.noreply.github.com>
2021-12-25 22:16:34 +0200

    Updates for WW2 loadouts.
    Major changes:
     - FW190A8 ground attack loadout WGr21 rockets (primarily an air-to-air weapon) replaced with SC500J or AB500 bombs.
     - FW190D9 WGr21 rockets replaced with R4M rocket packs and SC500 bombs for CAS.
     - Added droptanks for I-16.
     - Ju-88A4 CAS loadout changed for AB500 cluster bombs.
     - Mosquito now has rockets which were introduced in DCS World 2.7.9.17830 open beta
     - Added more/heavier bombs to P-47
     - P-51 now has a separate OCA/Runway loadout with bombs. Other ground attack loadouts switched to rockets.
    
    Also includes an SA342Minigun loadout which we didn't previously have.

--- resources/customized_payloads/FW-190D9.lua
+++ resources/customized_payloads/FW-190D9.lua
@@ -2,26 +2,8 @@ local unitPayloads = {
 	["name"] = "FW-190D9",
 	["payloads"] = {
 		[1] = {
-			["name"] = "ANTISHIP",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{WGr21}",
-					["num"] = 3,
-				},
-				[2] = {
-					["CLSID"] = "{WGr21}",
-					["num"] = 2,
-				},
-			},
-			["tasks"] = {
-				[1] = 11,
-				[2] = 10,
-				[3] = 32,
-				[4] = 31,
-			},
-		},
-		[2] = {
-			["name"] = "STRIKE",
+			["displayName"] = "Liberation Strike",
+			["name"] = "Liberation Strike",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "SC_501_SC500",
@@ -29,37 +11,154 @@ local unitPayloads = {
 				},
 			},
 			["tasks"] = {
-				[1] = 34,
-				[2] = 31,
-				[3] = 30,
-				[4] = 32,
+				[1] = 11,
 			},
 		},
-		[3] = {
-			["name"] = "CAS",
+		[2] = {
+			["displayName"] = "Liberation TARCAP",
+			["name"] = "Liberation TARCAP",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{WGr21}",
-					["num"] = 3,
+					["CLSID"] = "FW109_FUEL_TANK",
+					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{WGr21}",
+					["CLSID"] = "{FW_190_R4M_RGHT_WING}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{FW_190_R4M_LEFT_WING}",
 					["num"] = 2,
 				},
 			},
 			["tasks"] = {
 				[1] = 11,
-				[2] = 10,
-				[3] = 32,
-				[4] = 31,
+			},
+		},
+		[3] = {
+			["displayName"] = "Liberation CAS",
+			["name"] = "Liberation CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "SC_501_SC500",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
 			},
 		},
 		[4] = {
-			["name"] = "CAP",
+			["displayName"] = "Liberation Escort",
+			["name"] = "Liberation Escort",
 			["pylons"] = {
+				[1] = {
+					["CLSID"] = "FW109_FUEL_TANK",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{FW_190_R4M_RGHT_WING}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{FW_190_R4M_LEFT_WING}",
+					["num"] = 2,
+				},
 			},
 			["tasks"] = {
-				[1] = 31,
+				[1] = 11,
+			},
+		},
+		[5] = {
+			["name"] = "Liberation BARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "FW109_FUEL_TANK",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{FW_190_R4M_RGHT_WING}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{FW_190_R4M_LEFT_WING}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[6] = {
+			["displayName"] = "Liberation Fighter Sweep",
+			["name"] = "Liberation Fighter Sweep",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "FW109_FUEL_TANK",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{FW_190_R4M_RGHT_WING}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{FW_190_R4M_LEFT_WING}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[7] = {
+			["name"] = "Liberation BAI",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "SC_501_SC500",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[8] = {
+			["displayName"] = "Liberation DEAD",
+			["name"] = "Liberation DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "SC_501_SC500",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[9] = {
+			["displayName"] = "Liberation OCA/Aircraft",
+			["name"] = "Liberation OCA/Aircraft",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "ER_4_SC50",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[10] = {
+			["displayName"] = "Liberation OCA/Runway",
+			["name"] = "Liberation OCA/Runway",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "SC_501_SC500",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
 			},
 		},
 	},
