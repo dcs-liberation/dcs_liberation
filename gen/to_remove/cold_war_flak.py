f00a97446cffaf4b85259789070cbd57d312792e
RndName <mail@rndname.de>
2022-01-15 23:26:46 +0100

    Prepare the generators for template migration
    adopt armor and ship generators for randomization

---
+++ gen/to_remove/cold_war_flak.py
@@ -0,0 +1,153 @@
+import random
+
+from dcs.vehicles import AirDefence, Unarmed
+
+from game.theater.theatergroundobject import AirDefenseRange
+from gen.to_remove.airdefensegroupgenerator import (
+    AirDefenseGroupGenerator,
+)
+
+
+class EarlyColdWarFlakGenerator(AirDefenseGroupGenerator):
+    """
+    This generator attempt to mimic an early cold-war era flak AAA site.
+    The Flak 18 88mm is used as the main long range gun, S-60 is used as a mid range gun and 2 Bofors 40mm guns provide short range protection.
+
+    This does not include search lights and telemeter computer (Kdo.G 40) because these are paid units only available in WW2 asset pack
+    """
+
+    name = "Early Cold War Flak Site"
+
+    def generate(self) -> None:
+
+        spacing = random.randint(30, 60)
+        index = 0
+
+        # Long range guns
+        for i in range(3):
+            for j in range(2):
+                index = index + 1
+                self.add_unit(
+                    AirDefence.Flak18,
+                    "AAA#" + str(index),
+                    self.position.x + spacing * i + random.randint(1, 5),
+                    self.position.y + spacing * j + random.randint(1, 5),
+                    self.heading,
+                )
+
+        # Medium range guns
+        self.add_unit(
+            AirDefence.S_60_Type59_Artillery,
+            "SHO#1",
+            self.position.x - 40,
+            self.position.y - 40,
+            self.heading.opposite,
+        ),
+        self.add_unit(
+            AirDefence.S_60_Type59_Artillery,
+            "SHO#2",
+            self.position.x + spacing * 2 + 40,
+            self.position.y + spacing + 40,
+            self.heading,
+        ),
+
+        # Short range guns
+        self.add_unit(
+            AirDefence.ZU_23_Emplacement_Closed,
+            "SHO#3",
+            self.position.x - 80,
+            self.position.y - 40,
+            self.heading.opposite,
+        ),
+        self.add_unit(
+            AirDefence.ZU_23_Emplacement_Closed,
+            "SHO#4",
+            self.position.x + spacing * 2 + 80,
+            self.position.y + spacing + 40,
+            self.heading,
+        ),
+
+        # Add a truck
+        self.add_unit(
+            Unarmed.KAMAZ_Truck,
+            "Truck#",
+            self.position.x - 60,
+            self.position.y - 20,
+            self.heading,
+        )
+
+    @classmethod
+    def range(cls) -> AirDefenseRange:
+        return AirDefenseRange.AAA
+
+
+class ColdWarFlakGenerator(AirDefenseGroupGenerator):
+    """
+    This generator attempt to mimic a cold-war era flak AAA site.
+    The Flak 18 88mm is used as the main long range gun, 2 S-60 57mm gun improve mid range firepower, while 2 Zu-23 guns even provide short range protection.
+    The site is also fitted with a P-19 radar for early detection.
+    """
+
+    name = "Cold War Flak Site"
+
+    def generate(self) -> None:
+
+        spacing = random.randint(30, 60)
+        index = 0
+
+        # Long range guns
+        for i in range(3):
+            for j in range(2):
+                index = index + 1
+                self.add_unit(
+                    AirDefence.Flak18,
+                    "AAA#" + str(index),
+                    self.position.x + spacing * i + random.randint(1, 5),
+                    self.position.y + spacing * j + random.randint(1, 5),
+                    self.heading,
+                )
+
+        # Medium range guns
+        self.add_unit(
+            AirDefence.S_60_Type59_Artillery,
+            "SHO#1",
+            self.position.x - 40,
+            self.position.y - 40,
+            self.heading.opposite,
+        ),
+        self.add_unit(
+            AirDefence.S_60_Type59_Artillery,
+            "SHO#2",
+            self.position.x + spacing * 2 + 40,
+            self.position.y + spacing + 40,
+            self.heading,
+        ),
+
+        # Short range guns
+        self.add_unit(
+            AirDefence.ZU_23_Emplacement_Closed,
+            "SHO#3",
+            self.position.x - 80,
+            self.position.y - 40,
+            self.heading.opposite,
+        ),
+        self.add_unit(
+            AirDefence.ZU_23_Emplacement_Closed,
+            "SHO#4",
+            self.position.x + spacing * 2 + 80,
+            self.position.y + spacing + 40,
+            self.heading,
+        ),
+
+        # Add a P19 Radar for EWR
+        self.add_unit(
+            AirDefence.P_19_s_125_sr,
+            "SR#0",
+            self.position.x - 60,
+            self.position.y - 20,
+            self.heading,
+        )
+
+    @classmethod
+    def range(cls) -> AirDefenseRange:
+        return AirDefenseRange.AAA
---
+++ gen/to_remove/sam_sa5.py
@@ -0,0 +1,77 @@
+from typing import Type
+
+from dcs.unittype import VehicleType
+from dcs.vehicles import AirDefence, Unarmed
+
+from game import Game
+from game.theater import SamGroundObject
+from game.theater.theatergroundobject import AirDefenseRange
+from gen.to_remove.airdefensegroupgenerator import (
+    AirDefenseGroupGenerator,
+)
+
+
+class SA5Generator(AirDefenseGroupGenerator):
+    """
+    This generate a SA-5 group using the TinShield SR
+    """
+
+    name = "SA-5/S-200 Site"
+
+    def __init__(self, game: Game, ground_object: SamGroundObject):
+        super().__init__(game, ground_object)
+        self.sr: Type[VehicleType] = AirDefence.RLS_19J6
+
+    def generate(self) -> None:
+        self.add_unit(
+            self.sr,
+            "SR",
+            self.position.x,
+            self.position.y,
+            self.heading,
+        )
+        self.add_unit(
+            AirDefence.RPC_5N62V,
+            "TR",
+            self.position.x + 20,
+            self.position.y,
+            self.heading,
+        )
+        self.add_unit(
+            Unarmed.Ural_375,
+            "LOGI",
+            self.position.x - 20,
+            self.position.y,
+            self.heading,
+        )
+
+        num_launchers = 6
+        positions = self.get_circular_position(
+            num_launchers, launcher_distance=120, coverage=360
+        )
+
+        for i, position in enumerate(positions):
+            self.add_unit(
+                AirDefence.S_200_Launcher,
+                "LN#" + str(i),
+                position[0],
+                position[1],
+                position[2],
+            )
+
+    @classmethod
+    def range(cls) -> AirDefenseRange:
+        return AirDefenseRange.Long
+
+
+# TODO Replace this legacy handling once the Generators can be faction sensitive #1903
+class SA5FlatFaceGenerator(SA5Generator):
+    """
+    This generate a SA-5 group using the P-19 FlatFace SR
+    """
+
+    name = "SA-5/S-200 Site wit FlatFace SR"
+
+    def __init__(self, game: Game, ground_object: SamGroundObject):
+        super().__init__(game, ground_object)
+        self.sr: Type[VehicleType] = AirDefence.P_19_s_125_sr
