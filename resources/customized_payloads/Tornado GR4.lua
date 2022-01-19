faf559c549fa43e9b50224bda7b0460deb596c44
MetalStormGhost <ghosti@smytky.org>
2022-01-19 11:28:53 +0200

    Removed SEAD capability from the Tornado GR4 as suggested by @SnappyComebacks (Resolves #1960). The Tornado GR4 still retains its DEAD mission capability.

--- resources/customized_payloads/Tornado GR4.lua
+++ resources/customized_payloads/Tornado GR4.lua
@@ -185,62 +185,6 @@ local unitPayloads = {
 				[1] = 17,
 			},
 		},
-		[5] = {
-			["name"] = "SEAD",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{8C3F26A1-FA0F-11d5-9190-00A0249B6F00}",
-					["num"] = 12,
-				},
-				[2] = {
-					["CLSID"] = "{EF124821-F9BB-4314-A153-E0E2FE1162C4}",
-					["num"] = 11,
-				},
-				[3] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 10,
-				},
-				[4] = {
-					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
-					["num"] = 8,
-				},
-				[5] = {
-					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
-					["num"] = 7,
-				},
-				[6] = {
-					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
-					["num"] = 6,
-				},
-				[7] = {
-					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
-					["num"] = 5,
-				},
-				[8] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 3,
-				},
-				[9] = {
-					["CLSID"] = "{EF124821-F9BB-4314-A153-E0E2FE1162C4}",
-					["num"] = 2,
-				},
-				[10] = {
-					["CLSID"] = "{8C3F26A2-FA0F-11d5-9190-00A0249B6F00}",
-					["num"] = 1,
-				},
-				[11] = {
-					["CLSID"] = "{0D33DDAE-524F-4A4E-B5B8-621754FE3ADE}",
-					["num"] = 4,
-				},
-				[12] = {
-					["CLSID"] = "{0D33DDAE-524F-4A4E-B5B8-621754FE3ADE}",
-					["num"] = 9,
-				},
-			},
-			["tasks"] = {
-				[1] = 32,
-			},
-		},
 	},
 	["unitType"] = "Tornado GR4",
 }
