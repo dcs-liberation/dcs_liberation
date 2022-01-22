93a0db3112bbb06bfd88f8de9a09ea6ca579da7a
RndName <mail@rndname.de>
2022-01-22 13:05:04 +0100

    Add otion to ignore the empty install dir warning message
    Adds a do not show again checkbox to the empty install dir warning popup on lib start. This only appears when the install dir is empty to prevent mission scripting replacement.
    
    Also adds a warning message to the log when the dir is empty. Will help to identify this in bug reports.

--- qt_ui/liberation_install.py
+++ qt_ui/liberation_install.py
@@ -22,6 +22,7 @@ def init():
     global __dcs_saved_game_directory
     global __dcs_installation_directory
     global __last_save_file
+    global __ignore_empty_install_directory
 
     if PREFERENCES_PATH.exists():
         try:
@@ -31,14 +32,19 @@ def init():
             __dcs_saved_game_directory = pref_data["saved_game_dir"]
             __dcs_installation_directory = pref_data["dcs_install_dir"]
             __last_save_file = pref_data.get("last_save_file", "")
+            __ignore_empty_install_directory = pref_data.get(
+                "ignore_empty_install_directory", False
+            )
             is_first_start = False
         except KeyError:
             __dcs_saved_game_directory = ""
             __dcs_installation_directory = ""
             __last_save_file = ""
+            __ignore_empty_install_directory = False
             is_first_start = True
     else:
         __last_save_file = ""
+        __ignore_empty_install_directory = False
         try:
             __dcs_saved_game_directory = (
                 dcs.installation.get_dcs_saved_games_directory()
@@ -76,10 +82,12 @@ def save_config():
     global __dcs_saved_game_directory
     global __dcs_installation_directory
     global __last_save_file
+    global __ignore_empty_install_directory
     pref_data = {
         "saved_game_dir": __dcs_saved_game_directory,
         "dcs_install_dir": __dcs_installation_directory,
         "last_save_file": __last_save_file,
+        "ignore_empty_install_directory": __ignore_empty_install_directory,
     }
     PREFERENCES_PATH.parent.mkdir(exist_ok=True, parents=True)
     with PREFERENCES_PATH.open("w") as prefs:
@@ -94,6 +102,16 @@ def get_dcs_install_directory():
 def get_saved_game_dir():
     global __dcs_saved_game_directory
     return __dcs_saved_game_directory
+
+
+def ignore_empty_install_directory():
+    global __ignore_empty_install_directory
+    return __ignore_empty_install_directory
+
+
+def set_ignore_empty_install_directory(value: bool):
+    global __ignore_empty_install_directory
+    __ignore_empty_install_directory = value
 
 
 def get_last_save_file():
