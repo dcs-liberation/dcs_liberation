6f2249a08e08071fb4d31c903d25bfde08f0716b
MetalStormGhost <ghosti@smytky.org>
2022-01-25 17:58:14 +0200

    Changed the DEAD loadout to 2xALARM, 3xGBU-12, TGP, ECM pod, BOZ-107 countermeasure pod.

--- resources/customized_payloads/Tornado GR4.lua
+++ resources/customized_payloads/Tornado GR4.lua
@@ -210,12 +210,20 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[7] = {
-					["CLSID"] = "{0D33DDAE-524F-4A4E-B5B8-621754FE3ADE}",
-					["num"] = 9,
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 8,
 				},
 				[8] = {
-					["CLSID"] = "{0D33DDAE-524F-4A4E-B5B8-621754FE3ADE}",
-					["num"] = 4,
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 7,
+				},
+				[9] = {
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 6,
+				},
+				[10] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
 				},
 			},
 			["tasks"] = {
