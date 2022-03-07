54b9392d4bdcdd9ad6b385ba5b70ea5f65b69052
Dan Albert <dan@gingerhq.net>
2022-03-07 03:29:56 -0800

    Add missing front line click handlers.
    Fixes https://github.com/dcs-liberation/dcs_liberation/issues/2056

--- client/src/components/frontline/FrontLine.tsx
+++ client/src/components/frontline/FrontLine.tsx
@@ -1,4 +1,7 @@
-import { FrontLine as FrontLineModel } from "../../api/liberationApi";
+import {
+  FrontLine as FrontLineModel,
+  useOpenNewFrontLinePackageDialogMutation,
+} from "../../api/liberationApi";
 import { Polyline } from "react-leaflet";
 
 interface FrontLineProps {
@@ -6,8 +9,18 @@ interface FrontLineProps {
 }
 
 function FrontLine(props: FrontLineProps) {
+  const [openNewPackageDialog] = useOpenNewFrontLinePackageDialogMutation();
   return (
-    <Polyline positions={props.front.extents} weight={8} color={"#fe7d0a"} />
+    <Polyline
+      positions={props.front.extents}
+      weight={8}
+      color={"#fe7d0a"}
+      eventHandlers={{
+        contextmenu: () => {
+          openNewPackageDialog({ frontLineId: props.front.id });
+        },
+      }}
+    />
   );
 }
 
