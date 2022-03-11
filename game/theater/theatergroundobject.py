752d206e7f5e79df777763560868c3436daa9cbf
MetalStormGhost <ghosti@smytky.org>
2022-03-11 19:09:15 +0200

    Resolving a conflict in theatergroundobject.py imports.

--- game/theater/theatergroundobject.py
+++ game/theater/theatergroundobject.py
@@ -3,10 +3,12 @@ from __future__ import annotations
 import itertools
 import uuid
 from abc import ABC
-from typing import Any, Iterator, List, Optional, TYPE_CHECKING, Type
+from typing import Type
+from typing import Any, Iterator, List, Optional, TYPE_CHECKING
 
 from dcs.mapping import Point
-from dcs.unittype import VehicleType, ShipType
+from dcs.unittype import VehicleType
+from dcs.unittype import ShipType
 from shapely.geometry import Point as ShapelyPoint
 
 from game.sidc import (
