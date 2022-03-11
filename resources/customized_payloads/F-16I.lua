fb1ef05e1af1d536a378c096eeaa4dca3fbd799b
MetalStormGhost <ghosti@smytky.org>
2022-03-11 18:31:41 +0200

    F-16I banner and loadouts, courtesy of Schmokedpancake.

---
+++ resources/customized_payloads/F-16I.lua
@@ -0,0 +1,334 @@
+local unitPayloads = {
+	["name"] = "F-16I",
+	["payloads"] = {
+		[1] = {
+			["name"] = "CAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[2] = {
+			["name"] = "CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{DB769D48-67D7-42ED-A2BE-108D566C8B1E}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[10] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[3] = {
+			["displayName"] = "Liberation Strike",
+			["name"] = "Liberation Strike",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{GBU-31V3B}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{GBU-31V3B}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[10] = {
+					["CLSID"] = "{ANAAQ-13}",
+					["num"] = 10,
+				},
+				[11] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+			},
+			["tasks"] = {
+				[1] = 33,
+			},
+		},
+		[4] = {
+			["displayName"] = "Liberation Sead",
+			["name"] = "Liberation Sead",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{B06DD79A-F21E-4EB9-BD9D-AB3844618C93}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+			},
+			["tasks"] = {
+				[1] = 29,
+			},
+		},
+		[5] = {
+			["displayName"] = "Anti Ship",
+			["name"] = "Anti Ship",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[2] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 8,
+				},
+				[3] = {
+					["CLSID"] = "LAU_117_AGM_65G",
+					["num"] = 7,
+				},
+				[4] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 6,
+				},
+				[5] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+				[6] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 5,
+				},
+				[7] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "LAU_117_AGM_65G",
+					["num"] = 3,
+				},
+				[9] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 2,
+				},
+				[10] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 30,
+				[2] = 31,
+				[3] = 32,
+				[4] = 34,
+			},
+		},
+		[6] = {
+			["displayName"] = "Liberation Dead",
+			["name"] = "Liberation Dead",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 1,
+				},
+				[2] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 2,
+				},
+				[3] = {
+					["CLSID"] = "{AGM-154A}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 4,
+				},
+				[5] = {
+					["CLSID"] = "{IDF Mods Project 300gal}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{F376DBEE-4CAE-41BA-ADD9-B2910AC95DEC}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{AGM-154A}",
+					["num"] = 7,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-9L}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "{40EF17B7-F508-45de-8566-6FFECC0C1AB8}",
+					["num"] = 9,
+				},
+				[10] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 11,
+				},
+			},
+			["tasks"] = {
+				[1] = 32,
+			},
+		},
+		[7] = {
+			["name"] = "Liberation Ferry",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "MXU-648-TP",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "MXU-648-TP",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{600gal}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "MXU-648-TP",
+					["num"] = 7,
+				},
+				[7] = {
+					["CLSID"] = "<CLEAN>",
+					["num"] = 8,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+	},
+	["unitType"] = "F-16I",
+}
+return unitPayloads
