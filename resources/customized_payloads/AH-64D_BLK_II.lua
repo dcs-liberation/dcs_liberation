4d2289ece9ffd7cb59906977a2f1336e000306bf
walterroach <37820425+walterroach@users.noreply.github.com>
2022-03-18 13:02:12 -0500

    Add Starfire's loadouts for AH-64D_BLK_II

---
+++ resources/customized_payloads/AH-64D_BLK_II.lua
@@ -0,0 +1,80 @@
+local unitPayloads = {
+	["name"] = "AH-64D_BLK_II",
+	["payloads"] = {
+		[1] = {
+			["name"] = "Liberation CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{M261_M282}",
+					["num"] = 4,
+				},
+				[3] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{M261_M282}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[2] = {
+			["displayName"] = "Liberation BAI",
+			["name"] = "Liberation BAI",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 3,
+				},
+				[2] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 4,
+				},
+				[3] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 1,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[3] = {
+			["name"] = "Liberation OCA/Aircraft",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{M261_M229}",
+					["num"] = 4,
+				},
+				[2] = {
+					["CLSID"] = "{M261_M229}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{88D18A5E-99C8-4B04-B40B-1C02F2018B6E}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+	},
+	["unitType"] = "AH-64D_BLK_II",
+}
+return unitPayloads
