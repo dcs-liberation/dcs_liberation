526871292496a59a47db80fc29a27c475d2220f2
Raffson <Raffson@users.noreply.github.com>
2022-06-11 02:26:04 +0200

    Revert "Fixing double css import..."
    This reverts commit f426f7ab60d6d0a10f3eafc53503cef4d8aecb9c.

--- client/src/components/ruler/Ruler.tsx
+++ client/src/components/ruler/Ruler.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import L from "leaflet";
+import "leaflet-ruler/src/leaflet-ruler.css";
 import "leaflet-ruler";
 import "./Ruler.css"
 
