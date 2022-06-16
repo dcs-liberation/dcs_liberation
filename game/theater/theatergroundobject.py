c2f8d5096fdbc7ba97fb761e79983c0efdee3589
Raffson <Raffson@users.noreply.github.com>
2022-06-16 19:07:04 +0200

    FOB's got missed in commit 0238376...

--- game/theater/theatergroundobject.py
+++ game/theater/theatergroundobject.py
@@ -302,6 +302,8 @@ class BuildingGroundObject(TheaterGroundObject):
             entity = LandInstallationEntity.MAINTENANCE_FACILITY
         elif self.category == "farp":
             entity = LandInstallationEntity.HELICOPTER_LANDING_SITE
+        elif self.category == "fob":
+            entity = LandInstallationEntity.MILITARY_BASE
         elif self.category == "fuel":
             entity = LandInstallationEntity.WAREHOUSE_STORAGE_FACILITY
         elif self.category == "oil":
