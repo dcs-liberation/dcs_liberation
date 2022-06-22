20802114da071a78c9018cce6d8e45dad53b335b
Raffson <Raffson@users.noreply.github.com>
2022-06-22 21:55:54 +0200

    Temporarily comment out imports for CI -_-

--- client/src/api/eventstream.tsx
+++ client/src/api/eventstream.tsx
@@ -30,8 +30,8 @@ import { updateTgo } from "./tgosSlice";
 import { threatZonesUpdated } from "./threatZonesSlice";
 import { unculledZonesUpdated } from "./unculledZonesSlice";
 import { LatLng } from "leaflet";
-import { updateIadsConnection } from "./iadsNetworkSlice";
-import { IadsConnection } from "./_liberationApi";
+//import { updateIadsConnection } from "./iadsNetworkSlice";
+//import { IadsConnection } from "./_liberationApi";
 
 interface GameUpdateEvents {
   updated_flight_positions: { [id: string]: LatLng };
