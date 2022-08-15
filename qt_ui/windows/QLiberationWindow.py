be38969c405c6a07f5e257a883d9d6dde219a45d
Dan Albert <dan@gingerhq.net>
2022-08-14 18:30:22 -0700

    Update contributors list.

--- qt_ui/windows/QLiberationWindow.py
+++ qt_ui/windows/QLiberationWindow.py
@@ -437,6 +437,7 @@ class QLiberationWindow(QMainWindow):
             "Doc_of_Mur",
             "NickJZX",
             "Sith1144",
+            "Raffson",
         ]
         text = (
             "<h3>DCS Liberation "
