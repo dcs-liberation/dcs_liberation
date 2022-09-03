217bbc6f33cc3756235744e6c9f66b5e6f8cebbd
Dan Albert <dan@gingerhq.net>
2022-09-03 11:34:39 -0700

    Fix inclusion/exclusion zone reversal in UI.

--- client/src/components/terrainzones/TerrainZonesLayers.tsx
+++ client/src/components/terrainzones/TerrainZonesLayers.tsx
@@ -60,10 +60,10 @@ export default function TerrainZonesLayers() {
   return (
     <>
       <LayersControl.Overlay name="Inclusion zones">
-        {exclusion}
+        {inclusion}
       </LayersControl.Overlay>
       <LayersControl.Overlay name="Exclusion zones">
-        {inclusion}
+        {exclusion}
       </LayersControl.Overlay>
       <LayersControl.Overlay name="Sea zones">{sea}</LayersControl.Overlay>
     </>
