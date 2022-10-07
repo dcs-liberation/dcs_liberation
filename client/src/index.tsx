3a2eb182f97022cbe43cc785b0751069350fe7c8
Dan Albert <dan@gingerhq.net>
2022-10-07 14:31:19 -0700

    Fix the react app initialization.
    ReactDOM.render is deprecated and using it forces react 17 behavior.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,18 +4,20 @@ import { SocketProvider } from "./components/socketprovider/socketprovider";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <SocketProvider>
         <App />
       </SocketProvider>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
