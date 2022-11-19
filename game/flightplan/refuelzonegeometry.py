ec103e986bc5029ee918880097100fb5e1a3804d
Dan Albert <dan@gingerhq.net>
2022-11-19 15:12:23 -0800

    Add bug link for refuelzonegeometry.

--- game/flightplan/refuelzonegeometry.py
+++ game/flightplan/refuelzonegeometry.py
@@ -21,7 +21,8 @@ class RefuelZoneGeometry:
 
     def find_best_refuel_point(self) -> Point:
         # Do simple at first.
-        # TODO: Consider threats.
+        # TODO: Avoid threatened areas, minimize backtracking.
+        # https://github.com/dcs-liberation/dcs_liberation/issues/2542
         distance = 0.75 * self.package_home.distance_to_point(self.join)
         heading = self.package_home.heading_between_point(self.join)
         return self.package_home.point_from_heading(heading, distance)
