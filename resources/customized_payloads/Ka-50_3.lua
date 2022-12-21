d99ed6d2285afdb50809ca68d166506138c1e1ef
Dan Albert <dan@gingerhq.net>
2022-12-21 12:06:59 -0800

    Add BS3 loadouts.
    https://github.com/dcs-liberation/dcs_liberation/issues/2657
    (cherry picked from commit f2946817bfba7534ee68609f384078ccfb4ba67c)

---
+++ resources/customized_payloads/Ka-50_3.lua
@@ -0,0 +1,107 @@
+local unitPayloads = {
+	["name"] = "Ka-50_3",
+	["payloads"] = {
+		[1] = {
+			["name"] = "Liberation CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{9S846_2xIGLA}",
+					["num"] = 6,
+				},
+				[2] = {
+					["CLSID"] = "{9S846_2xIGLA}",
+					["num"] = 5,
+				},
+				[3] = {
+					["CLSID"] = "{A6FD14D3-6D30-4C85-88A7-8D17BEE120E2}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{A6FD14D3-6D30-4C85-88A7-8D17BEE120E2}",
+					["num"] = 1,
+				},
+				[5] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 3,
+				},
+				[6] = {
+					["CLSID"] = "{6A4B9E69-64FE-439a-9163-3A87FB6A4D81}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[2] = {
+			["displayName"] = "Liberation BAI",
+			["name"] = "Liberation BAI",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{9S846_2xIGLA}",
+					["num"] = 6,
+				},
+				[2] = {
+					["CLSID"] = "{9S846_2xIGLA}",
+					["num"] = 5,
+				},
+				[3] = {
+					["CLSID"] = "{6DADF342-D4BA-4D8A-B081-BA928C4AF86D}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{6DADF342-D4BA-4D8A-B081-BA928C4AF86D}",
+					["num"] = 1,
+				},
+				[5] = {
+					["CLSID"] = "{FC56DF80-9B09-44C5-8976-DCFAFF219062}",
+					["num"] = 3,
+				},
+				[6] = {
+					["CLSID"] = "{FC56DF80-9B09-44C5-8976-DCFAFF219062}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[3] = {
+			["displayName"] = "Liberation OCA/Aircraft",
+			["name"] = "Liberation OCA/Aircraft",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{9S846_2xIGLA}",
+					["num"] = 6,
+				},
+				[2] = {
+					["CLSID"] = "{9S846_2xIGLA}",
+					["num"] = 5,
+				},
+				[3] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 1,
+				},
+				[5] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 3,
+				},
+				[6] = {
+					["CLSID"] = "B_8V20A_OFP2",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+	},
+	["tasks"] = {
+	},
+	["unitType"] = "Ka-50_3",
+}
+return unitPayloads
