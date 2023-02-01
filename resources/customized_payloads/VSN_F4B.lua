37fed623a376861630845ce5ba5cf4e8e80783f4
Nosajthedevil <78634843+Nosajthedevil@users.noreply.github.com>
2023-02-01 10:26:57 -0600

    Add support for VSN F-4B F-4C mod
    Added support for VSN F-4 B&C Mod and added both to USA 1965 and USA 1975 factions and added F-4B to USN 1980 faction.

---
+++ resources/customized_payloads/VSN_F4B.lua
@@ -0,0 +1,224 @@
+local unitPayloads = {
+	["name"] = "VSN_F4C",
+	["payloads"] = {
+		[1] = {
+			["name"] = "CAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "VSN_F4EL_PTB",
+					["num"] = 2,
+				},
+				[2] = {
+					["CLSID"] = "LAU-105_2*AIM-9L",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 4,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "LAU-105_2*AIM-9L",
+					["num"] = 9,
+				},
+				[8] = {
+					["CLSID"] = "VSN_F4ER_PTB",
+					["num"] = 10,
+				},
+			},
+			["tasks"] = {
+				[1] = 18,
+				[2] = 19,
+				[3] = 11,
+			},
+		},
+		[2] = {
+			["name"] = "CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",
+					["num"] = 9,
+				},
+				[2] = {
+					["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 5,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 2,
+				},
+				[6] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 10,
+				},
+				[7] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "VSN_F4EC_PTB",
+					["num"] = 6,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[3] = {
+			["name"] = "SEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",
+					["num"] = 9,
+				},
+				[2] = {
+					["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 5,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 2,
+				},
+				[6] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 10,
+				},
+				[7] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "VSN_F4EC_PTB",
+					["num"] = 6,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[4] = {
+			["name"] = "STRIKE",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
+					["num"] = 9,
+				},
+				[2] = {
+					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 5,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
+					["num"] = 2,
+				},
+				[6] = {
+					["CLSID"] = "{AB8B8299-F1CC-4359-89B5-2172E0CF4A5A}",
+					["num"] = 10,
+				},
+				[7] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "VSN_F4EC_PTB",
+					["num"] = 6,
+				},
+			},
+			["tasks"] = {
+				[1] = 11,
+			},
+		},
+		[5] = {
+			["name"] = "ANTISHIP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",
+					["num"] = 9,
+				},
+				[2] = {
+					["CLSID"] = "{60CC734F-0AFA-4E2E-82B8-93B941AB11CF}",
+					["num"] = 3,
+				},
+				[3] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 5,
+				},
+				[4] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 7,
+				},
+				[5] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 2,
+				},
+				[6] = {
+					["CLSID"] = "{F3EFE0AB-E91A-42D8-9CA2-B63C91ED570A}",
+					["num"] = 10,
+				},
+				[7] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 4,
+				},
+				[8] = {
+					["CLSID"] = "{AIM-7F}",
+					["num"] = 8,
+				},
+				[9] = {
+					["CLSID"] = "VSN_F4EC_PTB",
+					["num"] = 6,
+				},
+			},			["tasks"] = {
+				[1] = 11,
+			},
+		},
+	},
+	["unitType"] = "VSN_F4C",
+}
+return unitPayloads
