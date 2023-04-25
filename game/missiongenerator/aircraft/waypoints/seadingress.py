3d946854d5f14047de3a7ce7eb4ece0a91398e7e
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2023-04-25 23:38:34 +1000

    back out changes for issue 2871

--- game/missiongenerator/aircraft/waypoints/seadingress.py
+++ game/missiongenerator/aircraft/waypoints/seadingress.py
@@ -52,21 +52,13 @@ class SeadIngressBuilder(PydcsWaypointBuilder):
                 # - Specify that DECOY weapon type is used in AttackGroup task so that
                 #   the flight actually launches the decoy. See link below for details
                 #   https://github.com/dcs-liberation/dcs_liberation/issues/2780
-                # - Set a stop condition of 60 seconds so that the flight does not continue
-                #   press the engagement as a DCS limitation means the RTB on winchester
-                #   does not work well with decoys. See link below for details.
-                #   https://github.com/dcs-liberation/dcs_liberation/issues/2781
-                #   This stop condition will allow the SwitchWaypoint task defined below
-                #   to kick in.
                 attack_task = AttackGroup(
                     miz_group.id,
                     weapon_type=DcsWeaponType.Decoy,
                     group_attack=True,
                     expend=Expend.All,
                 )
-                attack_task_control = ControlledTask(attack_task)
-                attack_task_control.stop_after_duration(120)
-                waypoint.tasks.append(attack_task_control)
+                waypoint.tasks.append(attack_task)
             else:
                 # All non ARM and non DECOY types will use the normal AttackGroup Task
                 attack_task = AttackGroup(
@@ -80,40 +72,3 @@ class SeadIngressBuilder(PydcsWaypointBuilder):
         # Preemptively use ECM to better avoid getting swatted.
         ecm_option = OptECMUsing(value=OptECMUsing.Values.UseIfDetectedLockByRadar)
         waypoint.tasks.append(ecm_option)
-
-        # For DECOY type flights, setup a waypoint task to skip the target waypoint after
-        # the attack task is complete. This is achieved using a switch waypoint task from the
-        # INGRESS point to the SPLIT point. This tasking prevents the flights continuing to
-        # overfly the target. See link below for the details of this issue
-        # https://github.com/dcs-liberation/dcs_liberation/issues/2781
-        if self.flight.loadout.has_weapon_of_type(WeaponType.DECOY):
-            ingress_index = None
-            split_index = None
-            if isinstance(self.flight.state, InFlight):
-                index = 2
-                # Handle special case where flight starts at the Ingress point
-                if self.flight.state.current_waypoint.name == "INGRESS":
-                    ingress_index = 1
-            else:
-                index = 1
-
-            for point in self.flight.flight_plan.waypoints:
-                # Skip waypoints that have already been passed for flights that are in flight already
-                if isinstance(
-                    self.flight.state, InFlight
-                ) and self.flight.state.has_passed_waypoint(point):
-                    continue
-                if point.name == "INGRESS":
-                    ingress_index = index
-                elif point.name == "SPLIT":
-                    split_index = index
-                index += 1
-            # If either the INGRESS or SPLIT waypoints cannot be found e.g. because they are
-            # modified by the user, do not try to add the SwitchWaypoint task.
-            if ingress_index is not None and split_index is not None:
-                switch_waypoint_task = SwitchWaypoint(ingress_index, split_index)
-                waypoint.tasks.append(switch_waypoint_task)
-            else:
-                logging.error(
-                    f"Could not find the Ingress or Split waypoint for SEAD mission {group.group_name}"
-                )
