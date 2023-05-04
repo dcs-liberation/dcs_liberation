54bdfc44d35fe8e26ee92a8ae86a3b85d20b8f76
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2023-05-04 23:05:41 +1000

    enable AEWC missions on FOBs

--- game/theater/controlpoint.py
+++ game/theater/controlpoint.py
@@ -1444,6 +1444,8 @@ class Fob(ControlPoint):
         if not self.is_friendly(for_player):
             yield FlightType.STRIKE
             yield FlightType.AIR_ASSAULT
+        else:
+            yield FlightType.AEWC
 
         yield from super().mission_types(for_player)
 
