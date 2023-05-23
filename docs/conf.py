76140178280ee95b7a844816d3270e3ef3ad37c2
Dan Albert <dan@gingerhq.net>
2023-05-23 01:31:53 -0700

    Bump version to 7.0.1.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "DCS Liberation"
 copyright = "2023, DCS Liberation Team"
 author = "DCS Liberation Team"
-release = "7.0.0"
+release = "7.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
