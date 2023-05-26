85fcb8e50fb86a787e0b00c84c7e3aebc26de761
Dan Albert <dan@gingerhq.net>
2023-05-25 22:20:52 -0700

    Fix formatting of takeoff time.
    (cherry picked from commit 257dabe4fa41c41bb86e0b88b2b0fda75ffbf007)

--- qt_ui/windows/mission/flight/settings/FlightPlanPropertiesGroup.py
+++ qt_ui/windows/mission/flight/settings/FlightPlanPropertiesGroup.py
@@ -93,7 +93,7 @@ class FlightPlanPropertiesGroup(QGroupBox):
             # is an invalid state for calling anything in TotEstimator.
             return
         self.departure_time.setText(
-            f"At {self.flight.flight_plan.startup_time():%H:%M%S}"
+            f"At {self.flight.flight_plan.startup_time():%H:%M:%S}"
         )
 
     def set_divert(self, index: int) -> None:
