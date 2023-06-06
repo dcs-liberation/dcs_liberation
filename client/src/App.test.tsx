fd5997dfece0154be5d5cebfafb533e3bcd09960
Dan Albert <dan@gingerhq.net>
2023-06-05 23:58:35 -0700

    Add tests for AircraftLayer.

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,11 +1,11 @@
 import App from "./App";
-import { store } from "./app/store";
+import { setupStore } from "./app/store";
 import { render } from "@testing-library/react";
 import { Provider } from "react-redux";
 
 test("app renders", () => {
   render(
-    <Provider store={store}>
+    <Provider store={setupStore()}>
       <App />
     </Provider>
   );
