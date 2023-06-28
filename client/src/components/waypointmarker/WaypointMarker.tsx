374759df0f45c1ab8fcd26037874e85fbaf386c6
Dan Albert <dan@gingerhq.net>
2023-06-27 22:24:37 -0700

    Test most of WaypointMarker.
    Unlike the other map tests which heavily rely on mocks, this one uses
    React refs to inspect the constructed leaflet objects. The DOM itself
    doesn't appear to contain anything worth testing against (react-leaflet
    rendering doesn't work like typical React rendering).
    
    This required some infrastructure changes:
    
    1. Forwarded ref from WaypointMarker to Marker so the test can observe
       it. Added a mergeRefs helper (and its own tests) to make that easier.
    2. Switched from identity-obj-proxy to jest-transform-stub, because the
       former doesn't produce a useable image for imports, and we need
       usable images for leaflet to be able to render.
    
    This doesn't yet test drag and drop behavior since that requires mocking
    the backend, and this commit is already complicated enough. That'll be
    next.

--- client/src/components/waypointmarker/WaypointMarker.tsx
+++ client/src/components/waypointmarker/WaypointMarker.tsx
@@ -3,12 +3,22 @@ import {
   Waypoint,
   useSetWaypointPositionMutation,
 } from "../../api/liberationApi";
+import mergeRefs from "../../mergeRefs";
 import { Icon } from "leaflet";
 import { Marker as LMarker } from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
-import { MutableRefObject, useCallback, useEffect, useRef } from "react";
+import {
+  ForwardedRef,
+  MutableRefObject,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import { Marker, Tooltip, useMap, useMapEvent } from "react-leaflet";
+
+export const TOOLTIP_ZOOM_LEVEL = 9;
 
 const WAYPOINT_ICON = new Icon({
   iconUrl: icon,
@@ -22,84 +32,84 @@ interface WaypointMarkerProps {
   flight: Flight;
 }
 
-const WaypointMarker = (props: WaypointMarkerProps) => {
-  // Most props of react-leaflet types are immutable and components will not
-  // update to account for changes, so we can't simply use the `permanent`
-  // property of the tooltip to control tooltip visibility based on the zoom
-  // level.
-  //
-  // On top of that, listening for zoom changes and opening/closing is not
-  // sufficient because clicking anywhere will close any opened tooltips (even
-  // if they are permanent; once openTooltip has been called that seems to no
-  // longer have any effect).
-  //
-  // Instead, listen for zoom changes and rebind the tooltip when the zoom level
-  // changes.
-  const map = useMap();
-  const marker: MutableRefObject<LMarker | undefined> = useRef();
+const WaypointMarker = forwardRef(
+  (props: WaypointMarkerProps, ref: ForwardedRef<LMarker>) => {
+    // Most props of react-leaflet types are immutable and components will not
+    // update to account for changes, so we can't simply use the `permanent`
+    // property of the tooltip to control tooltip visibility based on the zoom
+    // level.
+    //
+    // On top of that, listening for zoom changes and opening/closing is not
+    // sufficient because clicking anywhere will close any opened tooltips (even
+    // if they are permanent; once openTooltip has been called that seems to no
+    // longer have any effect).
+    //
+    // Instead, listen for zoom changes and rebind the tooltip when the zoom level
+    // changes.
+    const map = useMap();
+    const marker: MutableRefObject<LMarker | null> = useRef(null);
 
-  const [putDestination] = useSetWaypointPositionMutation();
+    const [putDestination] = useSetWaypointPositionMutation();
 
-  const rebindTooltip = useCallback(() => {
-    if (marker.current === undefined) {
-      return;
-    }
+    const rebindTooltip = useCallback(() => {
+      if (marker.current === null) {
+        return;
+      }
 
-    const tooltip = marker.current.getTooltip();
-    if (tooltip === undefined) {
-      return;
-    }
+      const tooltip = marker.current.getTooltip();
+      if (tooltip === undefined) {
+        return;
+      }
 
-    const permanent = map.getZoom() >= 9;
-    marker.current
-      .unbindTooltip()
-      .bindTooltip(tooltip, { permanent: permanent });
-  }, [map]);
-  useMapEvent("zoomend", rebindTooltip);
+      const permanent = map.getZoom() >= TOOLTIP_ZOOM_LEVEL;
+      marker.current
+        .unbindTooltip()
+        .bindTooltip(tooltip, { permanent: permanent });
+    }, [map]);
+    useMapEvent("zoomend", rebindTooltip);
 
-  useEffect(() => {
+    useEffect(() => {
+      const waypoint = props.waypoint;
+      marker.current?.setTooltipContent(
+        `${props.number} ${waypoint.name}<br />` +
+          `${waypoint.altitude_ft.toFixed()} ft ${
+            waypoint.altitude_reference
+          }<br />` +
+          waypoint.timing
+      );
+    });
+
     const waypoint = props.waypoint;
-    marker.current?.setTooltipContent(
-      `${props.number} ${waypoint.name}<br />` +
-        `${waypoint.altitude_ft.toFixed()} ft ${waypoint.altitude_reference}<br />` +
-        waypoint.timing
+    return (
+      <Marker
+        position={waypoint.position}
+        icon={WAYPOINT_ICON}
+        draggable
+        eventHandlers={{
+          dragstart: (e) => {
+            const m: LMarker = e.target;
+            m.setTooltipContent("Waiting to recompute TOT...");
+          },
+          dragend: async (e) => {
+            const m: LMarker = e.target;
+            const destination = m.getLatLng();
+            try {
+              await putDestination({
+                flightId: props.flight.id,
+                waypointIdx: props.number,
+                leafletPoint: { lat: destination.lat, lng: destination.lng },
+              });
+            } catch (e) {
+              console.error("Failed to set waypoint position", e);
+            }
+          },
+        }}
+        ref={mergeRefs(ref, marker)}
+      >
+        <Tooltip position={waypoint.position} />
+      </Marker>
     );
-  });
-
-  const waypoint = props.waypoint;
-  return (
-    <Marker
-      position={waypoint.position}
-      icon={WAYPOINT_ICON}
-      draggable
-      eventHandlers={{
-        dragstart: (e) => {
-          const m: LMarker = e.target;
-          m.setTooltipContent("Waiting to recompute TOT...");
-        },
-        dragend: async (e) => {
-          const m: LMarker = e.target;
-          const destination = m.getLatLng();
-          try {
-            await putDestination({
-              flightId: props.flight.id,
-              waypointIdx: props.number,
-              leafletPoint: { lat: destination.lat, lng: destination.lng },
-            });
-          } catch (e) {
-            console.error("Failed to set waypoint position", e);
-          }
-        },
-      }}
-      ref={(ref) => {
-        if (ref != null) {
-          marker.current = ref;
-        }
-      }}
-    >
-      <Tooltip position={waypoint.position} />
-    </Marker>
-  );
-};
+  }
+);
 
 export default WaypointMarker;
