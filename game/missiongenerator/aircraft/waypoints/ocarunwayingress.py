a5eeb837838d07cb5e1fce26f4af2c4773ab7446
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2023-07-18 10:07:01 +1000

    Fix anti-runway task generation for LGBs.
    Fixes https://github.com/dcs-liberation/dcs_liberation/issues/894.

--- game/missiongenerator/aircraft/waypoints/ocarunwayingress.py
+++ game/missiongenerator/aircraft/waypoints/ocarunwayingress.py
@@ -1,8 +1,14 @@
 import logging
 
 from dcs.point import MovingPoint
-from dcs.task import BombingRunway, OptFormation
+from dcs.task import (
+    Bombing,
+    BombingRunway,
+    OptFormation,
+    WeaponType as DcsWeaponType,
+)
 
+from game.data.weapons import WeaponType
 from game.theater import Airfield
 from .pydcswaypointbuilder import PydcsWaypointBuilder
 
@@ -17,7 +23,21 @@ class OcaRunwayIngressBuilder(PydcsWaypointBuilder):
             )
             return
 
-        waypoint.tasks.append(
-            BombingRunway(airport_id=target.airport.id, group_attack=True)
-        )
+        # The BombingRunway task in DCS does not use LGBs, which necessitates special handling
+        # by using the Bombing task instead. See https://github.com/dcs-liberation/dcs_liberation/issues/894
+        # for more details.
+        # The LGB work around assumes the Airfield position in DCS is on a runway, which seems
+        # to be the case for most if not all airfields.
+        if self.flight.loadout.has_weapon_of_type(WeaponType.LGB):
+            waypoint.tasks.append(
+                Bombing(
+                    position=target.position,
+                    group_attack=True,
+                    weapon_type=DcsWeaponType.Guided,
+                )
+            )
+        else:  # Use BombingRunway task for all other weapon types
+            waypoint.tasks.append(
+                BombingRunway(airport_id=target.airport.id, group_attack=True)
+            )
         waypoint.tasks.append(OptFormation.trail_open())
