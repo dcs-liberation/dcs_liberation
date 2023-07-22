416d2198de7a73fda00b590ecba8e85b1ad92cd2
Dan Albert <dan@gingerhq.net>
2023-07-22 16:31:19 -0700

    Add UI for acquiring/releasing TGP laser codes.

--- qt_ui/windows/mission/flight/payload/QFlightPayloadTab.py
+++ qt_ui/windows/mission/flight/payload/QFlightPayloadTab.py
@@ -16,6 +16,7 @@ from game.ato.flightmember import FlightMember
 from game.ato.loadouts import Loadout
 from qt_ui.widgets.QLabeledWidget import QLabeledWidget
 from .QLoadoutEditor import QLoadoutEditor
+from .ownlasercodeinfo import OwnLaserCodeInfo
 from .propertyeditor import PropertyEditor
 
 
@@ -89,6 +90,11 @@ class QFlightPayloadTab(QFrame):
         docsText.setAlignment(Qt.AlignCenter)
         docsText.setOpenExternalLinks(True)
 
+        self.own_laser_code_info = OwnLaserCodeInfo(
+            game, self.member_selector.selected_member
+        )
+        scrolling_layout.addLayout(self.own_laser_code_info)
+
         self.property_editor = PropertyEditor(
             self.flight, self.member_selector.selected_member
         )
@@ -117,6 +123,7 @@ class QFlightPayloadTab(QFrame):
         self.loadout_selector.setCurrentText(member.loadout.name)
         self.loadout_selector.setDisabled(member.loadout.is_custom)
         self.payload_editor.set_flight_member(member)
+        self.own_laser_code_info.set_flight_member(member)
         if self.member_selector.value() != 0:
             self.loadout_selector.setDisabled(
                 self.flight.use_same_loadout_for_all_members
