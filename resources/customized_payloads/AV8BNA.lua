d2b5eea0de68e2e7464fbd3e6d8bc23c727c6555
Starfire13 <72491792+Starfire13@users.noreply.github.com>
2023-12-28 09:05:43 +1100

    Update Harrier loadout (#3316)
    I had a look and the default Harrier loadouts were very outdated and
    quite sub-optimal. So I fixed it up.

--- resources/customized_payloads/AV8BNA.lua
+++ resources/customized_payloads/AV8BNA.lua
@@ -2,27 +2,36 @@ local unitPayloads = {
 	["name"] = "AV8BNA",
 	["payloads"] = {
 		[1] = {
-			["name"] = "INTERCEPT",
+			["displayName"] = "Liberation CAS",
+			["name"] = "Liberation CAS",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 8,
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
 				},
 				[2] = {
-					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
-					["num"] = 1,
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 3,
 				},
 				[3] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["CLSID"] = "{BRU-70A_3*GBU-54}",
 					["num"] = 2,
 				},
 				[4] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["CLSID"] = "{BRU-70A_3*GBU-54}",
 					["num"] = 7,
 				},
 				[5] = {
-					["CLSID"] = "{GAU_12_Equalizer}",
-					["num"] = 4,
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 8,
+				},
+				[7] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 1,
 				},
 			},
 			["tasks"] = {
@@ -30,7 +39,8 @@ local unitPayloads = {
 			},
 		},
 		[2] = {
-			["name"] = "SEAD",
+			["displayName"] = "Liberation SEAD",
+			["name"] = "Liberation SEAD",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "{AGM_122_SIDEARM}",
@@ -41,24 +51,16 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "LAU_117_AGM_65F",
+					["CLSID"] = "{LAU_7_AGM_122_SIDEARM}",
 					["num"] = 2,
 				},
 				[4] = {
-					["CLSID"] = "LAU_117_AGM_65F",
+					["CLSID"] = "{LAU_7_AGM_122_SIDEARM}",
 					["num"] = 7,
 				},
 				[5] = {
 					["CLSID"] = "{ALQ_164_RF_Jammer}",
 					["num"] = 5,
-				},
-				[6] = {
-					["CLSID"] = "LAU_117_AGM_65F",
-					["num"] = 6,
-				},
-				[7] = {
-					["CLSID"] = "LAU_117_AGM_65F",
-					["num"] = 3,
 				},
 			},
 			["tasks"] = {
@@ -66,7 +68,8 @@ local unitPayloads = {
 			},
 		},
 		[3] = {
-			["name"] = "CAS",
+			["displayName"] = "Liberation OCA/Runway",
+			["name"] = "Liberation OCA/Runway",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
@@ -77,28 +80,24 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "LAU_117_AGM_65F",
-					["num"] = 2,
-				},
-				[4] = {
-					["CLSID"] = "LAU_117_AGM_65F",
+					["CLSID"] = "{GBU_32_V_2B}",
 					["num"] = 7,
 				},
-				[5] = {
-					["CLSID"] = "{GAU_12_Equalizer}",
-					["num"] = 4,
+				[4] = {
+					["CLSID"] = "{GBU_32_V_2B}",
+					["num"] = 6,
 				},
-				[6] = {
+				[5] = {
 					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
 					["num"] = 5,
 				},
-				[7] = {
-					["CLSID"] = "{LAU-131 - 7 AGR-20 M282}",
-					["num"] = 6,
-				},
-				[8] = {
-					["CLSID"] = "{LAU-131 - 7 AGR-20 M282}",
+				[6] = {
+					["CLSID"] = "{GBU_32_V_2B}",
 					["num"] = 3,
+				},
+				[7] = {
+					["CLSID"] = "{GBU_32_V_2B}",
+					["num"] = 2,
 				},
 			},
 			["tasks"] = {
@@ -106,7 +105,119 @@ local unitPayloads = {
 			},
 		},
 		[4] = {
-			["name"] = "ANTISHIP",
+			["displayName"] = "Liberation BAI",
+			["name"] = "Liberation BAI",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{BRU-70A_3*GBU-54}",
+					["num"] = 7,
+				},
+				[4] = {
+					["CLSID"] = "{BRU-70A_2*GBU-54_RIGHT}",
+					["num"] = 6,
+				},
+				[5] = {
+					["CLSID"] = "{BRU-70A_2*GBU-54_LEFT}",
+					["num"] = 3,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-70A_3*GBU-54}",
+					["num"] = 2,
+				},
+				[7] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[5] = {
+			["displayName"] = "Liberation DEAD",
+			["name"] = "Liberation DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 6,
+				},
+				[4] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 3,
+				},
+				[5] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 7,
+				},
+				[7] = {
+					["CLSID"] = "LAU_117_AGM_65F",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[6] = {
+			["displayName"] = "Liberation Strike",
+			["name"] = "Liberation Strike",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
+				},
+				[3] = {
+					["CLSID"] = "{BRU-42_2*GBU-38_LEFT}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{BRU-42_3*GBU-38}",
+					["num"] = 2,
+				},
+				[5] = {
+					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 1,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-42_2*GBU-38_RIGHT}",
+					["num"] = 6,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-42_3*GBU-38}",
+					["num"] = 7,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[7] = {
+			["displayName"] = "Liberation Anti-ship",
+			["name"] = "Liberation Anti-ship",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
@@ -114,6 +225,43 @@ local unitPayloads = {
 				},
 				[2] = {
 					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{F16A4DE0-116C-4A71-97F0-2CF85B0313EC}",
+					["num"] = 7,
+				},
+				[4] = {
+					["CLSID"] = "{F16A4DE0-116C-4A71-97F0-2CF85B0313EC}",
+					["num"] = 6,
+				},
+				[5] = {
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
+				},
+				[6] = {
+					["CLSID"] = "{F16A4DE0-116C-4A71-97F0-2CF85B0313EC}",
+					["num"] = 3,
+				},
+				[7] = {
+					["CLSID"] = "{F16A4DE0-116C-4A71-97F0-2CF85B0313EC}",
+					["num"] = 2,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[8] = {
+			["displayName"] = "Liberation SEAD Escort",
+			["name"] = "Liberation SEAD Escort",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
+					["num"] = 8,
+				},
+				[2] = {
+					["CLSID"] = "{AGM_122_SIDEARM}",
 					["num"] = 1,
 				},
 				[3] = {
@@ -141,28 +289,9 @@ local unitPayloads = {
 				[1] = 31,
 			},
 		},
-		[5] = {
-			["name"] = "STRIKE",
-			["pylons"] = {
-				[1] = {
-					["CLSID"] = "{BRU-42_2*GBU-38_LEFT}",
-					["num"] = 2,
-				},
-				[2] = {
-					["CLSID"] = "{BRU-42_2*GBU-38_RIGHT}",
-					["num"] = 7,
-				},
-				[3] = {
-					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
-					["num"] = 5,
-				},
-			},
-			["tasks"] = {
-				[1] = 31,
-			},
-		},
-		[6] = {
-			["name"] = "CAP",
+		[9] = {
+			["displayName"] = "Liberation OCA/Aircraft",
+			["name"] = "Liberation OCA/Aircraft",
 			["pylons"] = {
 				[1] = {
 					["CLSID"] = "{6CEB49FC-DED8-4DED-B053-E1F033FF72D3}",
@@ -173,16 +302,24 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
-					["num"] = 2,
+					["CLSID"] = "{A111396E-D3E8-4b9c-8AC9-2432489304D5}",
+					["num"] = 5,
 				},
 				[4] = {
-					["CLSID"] = "{AIM-9M-ON-ADAPTER}",
+					["CLSID"] = "{GBU_32_V_2B}",
 					["num"] = 7,
 				},
 				[5] = {
-					["CLSID"] = "{GAU_12_Equalizer}",
-					["num"] = 4,
+					["CLSID"] = "{GBU_32_V_2B}",
+					["num"] = 6,
+				},
+				[6] = {
+					["CLSID"] = "{GBU_32_V_2B}",
+					["num"] = 3,
+				},
+				[7] = {
+					["CLSID"] = "{GBU_32_V_2B}",
+					["num"] = 2,
 				},
 			},
 			["tasks"] = {
