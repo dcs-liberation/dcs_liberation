2695f09ec6cca605c9fbe08c11071ae88d41f36d
Chilli935 <157713602+Chilli935@users.noreply.github.com>
2024-03-21 10:41:34 +0100

    Update QNewGameWizard.py
    Added mod version to newgamewizard

--- qt_ui/windows/newgame/QNewGameWizard.py
+++ qt_ui/windows/newgame/QNewGameWizard.py
@@ -884,7 +884,7 @@ class GeneratorOptions(QtWidgets.QWizardPage):
         modSettingsGroup.setLayout(modLayout)
         modLayout_row += 1
         modLayout.addWidget(
-            QtWidgets.QLabel("F/A-18EFG Super Hornet"), modLayout_row, 0
+            QtWidgets.QLabel("F/A-18EFG Super Hornet (version 2.2.5)"), modLayout_row, 0
         )
         modLayout.addWidget(fa18efg, modLayout_row, 1)
         modSettingsGroup.setLayout(modLayout)
