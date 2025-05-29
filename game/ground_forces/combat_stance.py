e8d05808f1b6741a878a7b642fb25b763f9bb687
monstertov <monstertov@RASPY2>
2025-05-29 13:14:48 +0200

    Addressed typo: changed 'aggresively' to 'aggressively'

--- game/ground_forces/combat_stance.py
+++ game/ground_forces/combat_stance.py
@@ -8,5 +8,5 @@ class CombatStance(Enum):
     )
     RETREAT = 2  # Unit will retreat
     BREAKTHROUGH = 3  # Unit will attempt a breakthrough, rushing forward very aggressively with big group of armored units, and even less armored units will move aggressively
-    ELIMINATION = 4  # Unit will progress aggresively toward anemy units, attempting to eliminate the ennemy force
+    ELIMINATION = 4  # Unit will progress aggressively toward anemy units, attempting to eliminate the ennemy force
     AMBUSH = 5  # Units will adopt a defensive stance a bit different from 'DEFENSIVE', ATGM & INFANTRY with RPG will be located on frontline with the armored units. (The groups of units will be smaller)
