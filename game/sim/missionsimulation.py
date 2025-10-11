5bcfea0e740df735bf080c42dea1bdc6a2c7ca3e
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2025-10-11 23:25:50 +1100

    Fix bug where UI does not show newly planned missions

--- game/sim/missionsimulation.py
+++ game/sim/missionsimulation.py
@@ -105,6 +105,9 @@ class MissionSimulation:
             self.game.blue.plan_missions(self.game.simulation_time)
             self.game.red.plan_missions(self.game.simulation_time)
             self.game.game_stats.update(self.game)
+            # Generate begin_new_turn event which triggers a refresh of the React map screen to
+            # show newly planned missions.
+            events.begin_new_turn()
 
     def finish(self) -> None:
         self.unit_map = None
