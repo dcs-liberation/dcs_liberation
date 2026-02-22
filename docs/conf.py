7cdfc10fcc42f9c6114e4afdb2d84ae71ec9c80c
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2026-02-22 22:12:01 +1100

    Bump version to 14.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "DCS Liberation"
 copyright = "2025, DCS Liberation Team"
 author = "DCS Liberation Team"
-release = "14.0.0"
+release = "14.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
