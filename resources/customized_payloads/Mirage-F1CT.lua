963e70af36d396e6cd866ba8614b4727dcc5142c
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2026-04-08 21:29:32 +1000

    Update Belouga CLSID

--- resources/customized_payloads/Mirage-F1CT.lua
+++ resources/customized_payloads/Mirage-F1CT.lua
@@ -51,11 +51,11 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 5,
 				},
 				[4] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 3,
 				},
 				[5] = {
@@ -63,11 +63,11 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[6] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 6,
 				},
 				[7] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 2,
 				},
 			},
@@ -314,11 +314,11 @@ local unitPayloads = {
 					["num"] = 1,
 				},
 				[3] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 5,
 				},
 				[4] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 3,
 				},
 				[5] = {
@@ -326,11 +326,11 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[6] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 6,
 				},
 				[7] = {
-					["CLSID"] = "{BLG66_BELOUGA}",
+					["CLSID"] = "{BLG66_AC}",
 					["num"] = 2,
 				},
 			},
