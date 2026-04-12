361e11a2e849481f31bc81ebe2c91667b1b0b8f0
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2026-04-12 12:02:34 +1000

    Update ECM pods on Su-34 to use most recent

--- resources/customized_payloads/Su-34.lua
+++ resources/customized_payloads/Su-34.lua
@@ -5,7 +5,7 @@ local unitPayloads = {
 			["name"] = "ANTISHIP",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 1,
 				},
 				[2] = {
@@ -49,7 +49,7 @@ local unitPayloads = {
 					["num"] = 11,
 				},
 				[12] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82A}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 12,
 				},
 			},
@@ -61,7 +61,7 @@ local unitPayloads = {
 			["name"] = "SEAD",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 1,
 				},
 				[2] = {
@@ -97,7 +97,7 @@ local unitPayloads = {
 					["num"] = 11,
 				},
 				[10] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82A}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 12,
 				},
 			},
@@ -109,7 +109,7 @@ local unitPayloads = {
 			["name"] = "CAS",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 1,
 				},
 				[2] = {
@@ -145,7 +145,7 @@ local unitPayloads = {
 					["num"] = 11,
 				},
 				[10] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82A}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 12,
 				},
 			},
@@ -157,7 +157,7 @@ local unitPayloads = {
 			["name"] = "STRIKE",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 1,
 				},
 				[2] = {
@@ -201,7 +201,7 @@ local unitPayloads = {
 					["num"] = 11,
 				},
 				[12] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82A}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 12,
 				},
 			},
@@ -213,7 +213,7 @@ local unitPayloads = {
 			["name"] = "CAP",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82F}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 1,
 				},
 				[2] = {
@@ -249,7 +249,7 @@ local unitPayloads = {
 					["num"] = 11,
 				},
 				[10] = {
-					["CLSID"] = "{44EE8698-89F9-48EE-AF36-5FD31896A82A}",
+					["CLSID"] = "{ECM_POD_L_175V}",
 					["num"] = 12,
 				},
 				[11] = {
