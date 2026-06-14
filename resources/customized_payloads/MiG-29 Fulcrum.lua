84b2eec44ff7895025d740ac894203440f19d0ca
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2026-06-14 21:35:41 +1000

    Support for DCS 2.9.27 (#3609)
    This PR introduces support for DCS 2.9.27.
    
    - Updates to weapons and hitpoint data.
    - Update Syria beacons
    - Add F-14A-95
    - Add F-100D

--- resources/customized_payloads/MiG-29 Fulcrum.lua
+++ resources/customized_payloads/MiG-29 Fulcrum.lua
@@ -1,32 +1,32 @@
 local unitPayloads = {
-	["name"] = "MiG-29A",
+	["name"] = "MiG-29 Fulcrum",
 	["payloads"] = {
 		[1] = {
 			["name"] = "CAS",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
-					["num"] = 7,
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
+					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "B-8M1 - 20 S-8OFP2",
-					["num"] = 5,
-				},
-				[3] = {
-					["CLSID"] = "B-8M1 - 20 S-8OFP2",
-					["num"] = 6,
-				},
-				[4] = {
-					["CLSID"] = "B-8M1 - 20 S-8OFP2",
-					["num"] = 3,
-				},
-				[5] = {
-					["CLSID"] = "B-8M1 - 20 S-8OFP2",
+					["CLSID"] = "{ROCKETS_S8-KOM_BDZ-UMK2-B}",
 					["num"] = 2,
 				},
+				[3] = {
+					["CLSID"] = "{ROCKETS_S8-KOM_BDZ-UMK2-B}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{ROCKETS_S8-KOM_BDZ-UMK2-B}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{ROCKETS_S8-KOM_BDZ-UMK2-B}",
+					["num"] = 6,
+				},
 				[6] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
-					["num"] = 1,
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
+					["num"] = 7,
 				},
 				[7] = {
 					["CLSID"] = "{PTB_1500_MIG29A}",
@@ -41,15 +41,15 @@ local unitPayloads = {
 			["name"] = "STRIKE",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{37DCC01E-9E02-432F-B61D-10C166CA2798}",
+					["CLSID"] = "{AB_FAB-500_BDZ-UMK2-B}",
 					["num"] = 2,
 				},
 				[3] = {
-					["CLSID"] = "{37DCC01E-9E02-432F-B61D-10C166CA2798}",
+					["CLSID"] = "{AB_FAB-500_BDZ-UMK2-B}",
 					["num"] = 3,
 				},
 				[4] = {
@@ -57,15 +57,15 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[5] = {
-					["CLSID"] = "{37DCC01E-9E02-432F-B61D-10C166CA2798}",
+					["CLSID"] = "{AB_FAB-500_BDZ-UMK2-B}",
 					["num"] = 5,
 				},
 				[6] = {
-					["CLSID"] = "{37DCC01E-9E02-432F-B61D-10C166CA2798}",
+					["CLSID"] = "{AB_FAB-500_BDZ-UMK2-B}",
 					["num"] = 6,
 				},
 				[7] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 7,
 				},
 			},
@@ -77,27 +77,27 @@ local unitPayloads = {
 			["name"] = "CAP",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 2,
 				},
 				[3] = {
-					["CLSID"] = "{9B25D316-0434-4954-868F-D51DB1A38DF0}",
+					["CLSID"] = "{MISSILE_R-27R_APU-470}",
 					["num"] = 3,
 				},
 				[4] = {
-					["CLSID"] = "{9B25D316-0434-4954-868F-D51DB1A38DF0}",
+					["CLSID"] = "{MISSILE_R-27R_APU-470}",
 					["num"] = 5,
 				},
 				[5] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 6,
 				},
 				[6] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 7,
 				},
 				[7] = {
@@ -113,28 +113,28 @@ local unitPayloads = {
 			["name"] = "ANTISHIP",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
-					["num"] = 7,
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
+					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
-					["num"] = 5,
-				},
-				[3] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
-					["num"] = 6,
-				},
-				[4] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
-					["num"] = 3,
-				},
-				[5] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
 					["num"] = 2,
 				},
+				[3] = {
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
+					["num"] = 6,
+				},
 				[6] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
-					["num"] = 1,
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
+					["num"] = 7,
 				},
 				[7] = {
 					["CLSID"] = "{PTB_1500_MIG29A}",
@@ -149,28 +149,28 @@ local unitPayloads = {
 			["name"] = "SEAD",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
-					["num"] = 7,
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
+					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
-					["num"] = 5,
-				},
-				[3] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
-					["num"] = 6,
-				},
-				[4] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
-					["num"] = 3,
-				},
-				[5] = {
-					["CLSID"] = "{3858707D-F5D5-4bbb-BDD8-ABB0530EBC7C}",
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
 					["num"] = 2,
 				},
+				[3] = {
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
+					["num"] = 3,
+				},
+				[4] = {
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
+					["num"] = 5,
+				},
+				[5] = {
+					["CLSID"] = "{ROCKETS_S24B_BDZ-UMK2-B}",
+					["num"] = 6,
+				},
 				[6] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
-					["num"] = 1,
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
+					["num"] = 7,
 				},
 				[7] = {
 					["CLSID"] = "{PTB_1500_MIG29A}",
@@ -186,15 +186,15 @@ local unitPayloads = {
 			["name"] = "OCA/Runway",
 			["pylons"] = {
 				[1] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 1,
 				},
 				[2] = {
-					["CLSID"] = "{BD289E34-DF84-4C5E-9220-4B14C346E79D}",
+					["CLSID"] = "{AB_BETAB-500SHP_BDZ-UMK2-B}",
 					["num"] = 2,
 				},
 				[3] = {
-					["CLSID"] = "{BD289E34-DF84-4C5E-9220-4B14C346E79D}",
+					["CLSID"] = "{AB_BETAB-500SHP_BDZ-UMK2-B}",
 					["num"] = 3,
 				},
 				[4] = {
@@ -202,15 +202,15 @@ local unitPayloads = {
 					["num"] = 4,
 				},
 				[5] = {
-					["CLSID"] = "{BD289E34-DF84-4C5E-9220-4B14C346E79D}",
+					["CLSID"] = "{AB_BETAB-500SHP_BDZ-UMK2-B}",
 					["num"] = 5,
 				},
 				[6] = {
-					["CLSID"] = "{BD289E34-DF84-4C5E-9220-4B14C346E79D}",
+					["CLSID"] = "{AB_BETAB-500SHP_BDZ-UMK2-B}",
 					["num"] = 6,
 				},
 				[7] = {
-					["CLSID"] = "{FBC29BFE-3D24-4C64-B81D-941239D12249}",
+					["CLSID"] = "{MISSILE_R-73_APU-73}",
 					["num"] = 7,
 				},
 			},
