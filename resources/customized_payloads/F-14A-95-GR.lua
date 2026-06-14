b951bf19e77168faa484656a1acc3c7c28b0dd5c
zhexu14 <64713351+zhexu14@users.noreply.github.com>
2026-06-14 14:13:23 +1000

    Support DCS 2.9.27

---
+++ resources/customized_payloads/F-14A-95-GR.lua
@@ -0,0 +1,287 @@
+local unitPayloads = {
+	["name"] = "F-14A (Export)",
+	["payloads"] = {
+		[1] = {
+			["name"] = "Liberation BARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM_54A_Mk47 L}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM_54A_Mk47 R}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 10,
+			},
+		},
+		[2] = {
+			["name"] = "Liberation CAS",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 10,
+			},
+		},
+		[3] = {
+			["name"] = "Liberation Strike",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 10,
+			},
+		},
+		[4] = {
+			["name"] = "Liberation BAI",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 10,
+			},
+		},
+		[5] = {
+			["name"] = "Liberation Anti-ship",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{BRU-32 MK-84}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 10,
+			},
+		},
+		[6] = {
+			["name"] = "Liberation DEAD",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM-7E}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{BRU-32 MK-20}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+		[7] = {
+			["name"] = "Liberation TARCAP",
+			["pylons"] = {
+				[1] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 10,
+				},
+				[2] = {
+					["CLSID"] = "{LAU-7 wtip - AIM-9P}",
+					["num"] = 1,
+				},
+				[3] = {
+					["CLSID"] = "{SHOULDER AIM_54A_Mk47 L}",
+					["num"] = 2,
+				},
+				[4] = {
+					["CLSID"] = "{SHOULDER AIM_54A_Mk47 R}",
+					["num"] = 9,
+				},
+				[5] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 7,
+				},
+				[6] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 4,
+				},
+				[7] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 6,
+				},
+				[8] = {
+					["CLSID"] = "{AIM_54A_Mk47}",
+					["num"] = 5,
+				},
+			},
+			["tasks"] = {
+				[1] = 31,
+			},
+		},
+	},
+	["unitType"] = "F-14A-95-GR",
+}
+return unitPayloads
